94eb34613bbcc9eb096af8b2c87259d095dd5794
pauld.mccarthy <pauld.mccarthy@e4b457b6-3699-3e31-c660-623cc9554e38>
2009-05-26 06:00:40 +0000

    -improved management of state; it's still pretty ugly, but i'm hoping that there will not be a need for more uglification

--- trunk/FlowManager/flowManager.m
+++ trunk/FlowManager/flowManager.m
@@ -32,10 +32,12 @@ function flowManager()
 % INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
 % CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-% POSSIBILITY OF SUCH DAMAGE.`
+% POSSIBILITY OF SUCH DAMAGE.
+
   rawData      = {};
   autoQCData   = {};
-  manualQCData = {};
+  
+  lastAutoQCSetIdx = 0;
 
   % import data
   [fieldTrip rawData skipped] = importManager();
@@ -96,38 +98,71 @@ function flowManager()
   function sample_data = rawDataRequestCallback()
   %RAWDATAREQUESTCALLBACK Called when raw data is needed. Returns the raw
   %data set.
-    
+  
     sample_data = rawData;
   end
-
-  function sample_data = autoQCRequestCallback()
+  
+  function sample_data = autoQCRequestCallback(setIdx, stateChange)
   %AUTOQCREQUESTCALLBACK Called when the user chooses to run auto QC
-  % routines over the data. Delegates to the autoQCManager.
+  % routines over the data. Delegates to the autoQCManager. The first time
+  % this function is called, the QC routines are executed over every data
+  % set. On subsequent calls, if the provided stateChange parameter is true,
+  % the existing QC data is returned. Otherwise, if the provided setIdx 
+  % parameter is the same as that previously passed in, the routines are 
+  % only executed on the set with the given index, and only after the user 
+  % confirms that they wish to overwrite the previous QC modifications.
   %
     sample_data = autoQCData;
     
+    % save data set selection
+    lastSetIdx = lastAutoQCSetIdx;
+    lastAutoQCSetIdx = setIdx;
+    
+    % user can disable prompt via qcPrompt toolbox 
+    % property - QC data can never be overwritten
     qcPrompt = true;
     try
       qcPrompt = str2num(readToolboxProperty('flowManager.qcPrompt'));
     catch e
     end
-    
-    % if QC has already been executed prompt user if they want to keep old
-    % QC data, or redo QC
-    if ~isempty(autoQCData) 
+
+    % if QC has not been run before, run QC over every data set
+    if isempty(autoQCData)
       
-      % user can disable prompt via qcPrompt toolbox property - data is
-      % never overwritten
+      setIdx = 1:length(rawData);
+      sample_data = rawData;
+    
+    % if just a state  change, return previous QC data
+    elseif stateChange, return;
+      
+    % if QC has already been executed, passed-in set index is the same as 
+    % that which was previoously passed, and state has not changed, prompt 
+    % user if they want to keep old QC data, or redo QC, for this set only
+    elseif lastSetIdx == setIdx
+      
       if ~qcPrompt, return; end
       
       response = questdlg(...
-        'Re-run auto-QC routines (existing flags/mods will be discarded)?');
+        'Re-run auto-QC routines (existing flags/mods will be discarded)?',...
+        'Re-run QC Routines?', 'Yes', 'No', 'No');
       
       if ~strcmp(response, 'Yes'), return; end
+    
+    % otherwise return the existing auto QC data
+    else return;
     end
+    
+    % save data set selection
+    lastSetIdx = setIdx;
 
     % run QC routines over raw data
-    autoQCData = autoQCManager(rawData);
+    aqc = autoQCManager(rawData(setIdx));
+    
+    % if user interrupted process, return either raw data or old QC data
+    if isempty(aqc), return; end
+    
+    % otherwise return new QC data
+    autoQCData(setIdx) = aqc;
     sample_data = autoQCData;
   end
 
