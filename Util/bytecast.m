844957dabe7539f18cb6b795d772a3b7aea0e87f
pauld.mccarthy <pauld.mccarthy@e4b457b6-3699-3e31-c660-623cc9554e38>
2009-07-23 03:11:10 +0000

    -the bytecast function was previously a subfunction within readWorkhorseEnsembles. The (soon to be added) readParadoppStructures function also makes use of bytecast, so it has been put in a separate file.

---
+++ Util/bytecast.m
@@ -0,0 +1,58 @@
+function value = bytecast(bytes, endianness, dataType)
+%BYTECAST Cast a vector of bytes to the given type. 
+%
+% This function is used by the Nortek Paradopp and Teledyne workhorse ADCP
+% parsers. Raw data files read in by both of these parsers store data in
+% little endian.
+% 
+% Inputs:
+%   bytes      - vector of bytes
+%   endianness - endianness of the bytes - 'L' for little, 'B' for big.
+%   dataType   - type to cast to, e.g. 'uint8', 'int64' etc.
+%
+% Outputs:
+%   value      - the given bytes cast to the given value.
+%
+% Author: Paul McCarthy <paul.mccarthy@csiro.au>
+%
+
+%
+% Copyright (c) 2009, eMarine Information Infrastructure (eMII) and Integrated 
+% Marine Observing System (IMOS).
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are met:
+% 
+%     * Redistributions of source code must retain the above copyright notice, 
+%       this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in the 
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of the eMII/IMOS nor the names of its contributors 
+%       may be used to endorse or promote products derived from this software 
+%       without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
+%
+
+  [m,c,cpuEndianness] = computer;
+  if cpuEndianness == endianness, value = typecast(bytes, dataType);
+    
+  % WILL NOT WORK IF MULTIPLE VALUES ARE PASSED IN. 
+  % Not a problem at the moment though (for x86)
+  else                            value = typecast(bytes(end:-1:1), dataType);
+  end
+  
+  value = double(value);
+end
