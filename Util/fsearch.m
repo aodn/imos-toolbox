db6a81dd38aaccfd8f13b2a6617f537f931f2d81
pauld.mccarthy <pauld.mccarthy@e4b457b6-3699-3e31-c660-623cc9554e38>
2009-07-27 23:46:33 +0000

    -fsearch function, previously a subfunction in importManager, now has a use outside of importManager

---
+++ Util/fsearch.m
@@ -0,0 +1,78 @@
+function hits = fsearch(pattern, root)
+%FSEARCH Recursive file/directory search.
+%
+% Performs a recursive search starting at the given root directory; returns
+% the names of all files and directories below the root which contain the 
+% given pattern as a substring with their name. The name comparison is case 
+% insensitive for alphabetical characters.
+%
+% Inputs:
+%
+%   pattern - Pattern to match.
+%
+%   root    - Directory from which to start the search.
+%
+% Outputs:
+% 
+%   hits    - Cell array of strings containing the files/directories that
+%             have a name which contains the pattern.
+%
+% Author: Paul McCarthy <paul.mccarthy@csiro.au>
+%
+
+%
+% Copyright (c) 2009, eMarine Information Infrastructure (eMII) and Integrated 
+% Marine Observing System (IMOS).
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are met:
+% 
+%     * Redistributions of source code must retain the above copyright notice, 
+%       this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in the 
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of the eMII/IMOS nor the names of its contributors 
+%       may be used to endorse or promote products derived from this software 
+%       without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
+%
+  hits = {};
+  
+  if ~isdir(root), return; end
+  
+  entries = dir(root);
+  
+  for k = 1:length(entries)
+    
+    d = entries(k);
+    
+    % ignore current/prev entries
+    if strcmp(d.name, '.') || strcmp(d.name, '..'), continue; end
+    
+    % compare file and directory names against pattern
+    if strfind(lower(d.name), lower(pattern))
+      
+      hits{end+1} = [root filesep d.name];
+    end
+    
+    % recursively search subdirectories
+    if d.isdir, 
+      
+      subhits = fsearch(pattern, [root filesep d.name]);
+      hits = [hits subhits];
+    end
+  end
+end
