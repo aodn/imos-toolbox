0aafc56db376239883c460e3d6a9cb22cfb66e32
pauld.mccarthy <pauld.mccarthy@e4b457b6-3699-3e31-c660-623cc9554e38>
2009-09-15 22:20:00 +0000

    -graph functions for Transect graph type - flag display/interaction now works

--- trunk/Graph/Transect/getSelectedTransectGeneric.m
+++ trunk/Graph/Transect/getSelectedTransectGeneric.m
@@ -1,16 +1,22 @@
-function flags = flagTransect( parent, graphs, sample_data, vars )
-%FLAGTRANSECT Overlays flags for the given sample data variables on the 
-% given transect graphs.
+function dataIdx = getSelectedTransectGeneric( ...
+  sample_data, var, ax, highlight, click )
+%GETSELECTEDTRANSECTGENERIC Returns the indices of the currently selected 
+% (highlighted) data on the given axis.
+%
+% This function is nearly identical to
+% Graph/TimeSeries/getSelectedTimeSeriesGeneric.m.
 %
 % Inputs:
-%   parent      - handle to parent figure/uipanel.
-%   graphs      - vector handles to axis objects (one for each variable).
-%   sample_data - struct containing the sample data.
-%   vars        - vector of indices into the sample_data.variables array.
-%                 Must be the same length as graphs.
+%   sample_data - Struct containing the data set.
+%   var         - Variable in question (index into sample_data.variables).
+%   ax          - Axis in question.
+%   highlight   - Handle to the highlight object.
+%   click       - Where the user clicked the mouse.
+%   
 %
 % Outputs:
-%   flag        - handles to line objects that make up the flag overlays.
+%   dataIdx     - Vector of indices into the data, defining the indices
+%                 which are selected (and which were clicked on).
 %
 % Author: Paul McCarthy <paul.mccarthy@csiro.au>
 %
@@ -44,54 +50,40 @@ function flags = flagTransect( parent, graphs, sample_data, vars )
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 % POSSIBILITY OF SUCH DAMAGE.
 %
+error(nargchk(5,5,nargin));
 
-error(nargchk(4,4,nargin));
+if ~isstruct(sample_data), error('sample_data must be a struct');        end
+if ~isnumeric(var),        error('var must be numeric');                 end
+if ~ishandle(ax),          error('ax must be a graphics handle');        end
+if ~ishandle(highlight),   error('highlight must be a graphics handle'); end
+if ~isnumeric(click),      error('click must be numeric');               end
 
-if ~ishandle(parent),      error('parent must be a graphic handle');    end
-if ~ishandle(graphs),      error('graphs must be a graphic handle(s)'); end
-if ~isstruct(sample_data), error('sample_data must be a struct');       end
-if ~isnumeric(vars),       error('vars must be a numeric');             end
+dataIdx = [];
 
-flags = [];
-
-if isempty(vars), return; end
-
-% fail if there is no latitude/longitude data
 lat = getVar(sample_data.variables, 'LATITUDE');
 lon = getVar(sample_data.variables, 'LONGITUDE');
 
-if lat == 0 || lon == 0
-  error('data set contains no latitude/longitude data'); 
-end
+lat = sample_data.variables{lat};
+lon = sample_data.variables{lon};
 
-vars(vars == lat) = [];
-vars(vars == lon) = [];
-if isempty(vars), return; end
+highlightX = get(highlight, 'XData');
+highlightY = get(highlight, 'YData');
 
-hold on;
+% figure out if the click was anywhere near the highlight 
+% (within 1% of the current visible range on x and y)
+xError = get(ax, 'XLim');
+xError = abs(xError(1) - xError(2));
+yError = get(ax, 'YLim');
+yError = abs(yError(1) - yError(2));
 
-for k = 1:length(vars)
+% was click near highlight?
+if any(abs(click(1)-highlightX) <= xError*0.01)...
+&& any(abs(click(2)-highlightY) <= yError*0.01)
+
+  % find the indices of the selected points
+  highlightX = find(ismember(lat.data, highlightX));
+  highlightY = find(ismember(lon.data, highlightY));
   
-  % apply the flag function for this variable
-  flagFunc = ...
-    getGraphFunc('Transect', 'flag', sample_data.variables{vars(k)}.name);
-  f = flagFunc(graphs(k), sample_data, vars(k));
+  dataIdx = intersect(highlightX, highlightY);
   
-  % if the flag function returned nothing, insert a dummy handle 
-  if isempty(f), f = 0.0; end
-  
-  %
-  % the following is some ugly code which takes the flag handle(s) returned
-  % from the variable-specific flag function, and saves it/them in the 
-  % flags matrix, accounting for differences in size.
-  %
-  
-  fl = length(f);
-  fs = size(flags,2);
-  
-  if     fl > fs, flags(:,fs+1:fl) = 0.0;
-  elseif fl < fs, f    (  fl+1:fs) = 0.0;
-  end
-  
-  flags(k,:) = f;
 end
