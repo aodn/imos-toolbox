25449edce7f2ab8fa4012616ac44dcc01b769aa1
pauld.mccarthy <pauld.mccarthy@e4b457b6-3699-3e31-c660-623cc9554e38>
2009-11-03 02:47:34 +0000

    -Nortek ADCP parsers now provide backscatter data

--- Parser/awacParse.m
+++ Parser/awacParse.m
@@ -117,21 +117,21 @@ for k = 1:nsamples
   
   st = structures{k+3};
   
-  time(k)         = st.Time;
-  analn1(k)       = st.Analn1;
-  battery(k)      = st.Battery;
-  analn2(k)       = st.Analn2;
-  heading(k)      = st.Heading;
-  pitch(k)        = st.Pitch;
-  roll(k)         = st.Roll;
-  pressure(k)     = st.PressureMSB*65536 + st.PressureLSW;
-  temperature(k)  = st.Temperature;
-  velocity1(k,:)  = st.Vel1;
-  velocity2(k,:)  = st.Vel2;
-  velocity3(k,:)  = st.Vel3;
-  amplitude1(k,:) = st.Amp1;
-  amplitude2(k,:) = st.Amp2;
-  amplitude3(k,:) = st.Amp3;
+  time(k)           = st.Time;
+  analn1(k)         = st.Analn1;
+  battery(k)        = st.Battery;
+  analn2(k)         = st.Analn2;
+  heading(k)        = st.Heading;
+  pitch(k)          = st.Pitch;
+  roll(k)           = st.Roll;
+  pressure(k)       = st.PressureMSB*65536 + st.PressureLSW;
+  temperature(k)    = st.Temperature;
+  velocity1(k,:)    = st.Vel1;
+  velocity2(k,:)    = st.Vel2;
+  velocity3(k,:)    = st.Vel3;
+  backscatter1(k,:) = st.Amp1;
+  backscatter2(k,:) = st.Amp2;
+  backscatter3(k,:) = st.Amp3;
 end
 
 % battery     / 10.0   (0.1 V    -> V)
@@ -141,6 +141,7 @@ end
 % pressure    / 1000.0 (mm       -> m)   assuming equivalence to dbar
 % temperature / 100.0  (0.01 deg -> deg)
 % velocities  / 1000.0 (mm/s     -> m/s) assuming earth coordinates
+% backscatter * 0.45   (counts   -> dB)
 battery     = battery     / 10.0;
 heading     = heading     / 10.0;
 pitch       = pitch       / 10.0;
@@ -150,6 +151,9 @@ temperature = temperature / 100.0;
 velocity1   = velocity1   / 1000.0;
 velocity2   = velocity2   / 1000.0;
 velocity3   = velocity3   / 1000.0;
+backscatter1 = backscatter1 * 0.45;
+backscatter2 = backscatter2 * 0.45;
+backscatter3 = backscatter3 * 0.45;
 
 sample_data = struct;
 
@@ -161,39 +165,48 @@ sample_data.meta.instrument_model     = 'AWAC';
 sample_data.meta.instrument_serial_no = hardware.SerialNo;
 sample_data.meta.instrument_firmware  = hardware.FWversion;
 
-sample_data.dimensions{1}.name = 'TIME';
-sample_data.dimensions{2}.name = 'DEPTH';
-sample_data.variables {1}.name = 'VCUR';
-sample_data.variables {2}.name = 'UCUR';
-sample_data.variables {3}.name = 'ZCUR';
-sample_data.variables {4}.name = 'TEMP';
-sample_data.variables {5}.name = 'PRES';
-sample_data.variables {6}.name = 'VOLT';
-sample_data.variables {7}.name = 'PITCH';
-sample_data.variables {8}.name = 'ROLL';
-sample_data.variables {9}.name = 'HEADING';
+sample_data.dimensions{1} .name = 'TIME';
+sample_data.dimensions{2} .name = 'DEPTH';
+sample_data.variables {1} .name = 'VCUR';
+sample_data.variables {2} .name = 'UCUR';
+sample_data.variables {3} .name = 'ZCUR';
+sample_data.variables {4} .name = 'ABSI_1';
+sample_data.variables {5} .name = 'ABSI_2';
+sample_data.variables {6} .name = 'ABSI_3';
+sample_data.variables {7} .name = 'TEMP';
+sample_data.variables {8} .name = 'PRES';
+sample_data.variables {9} .name = 'VOLT';
+sample_data.variables {10}.name = 'PITCH';
+sample_data.variables {11}.name = 'ROLL';
+sample_data.variables {12}.name = 'HEADING';
 
-sample_data.variables {1}.dimensions = [1 2];
-sample_data.variables {2}.dimensions = [1 2];
-sample_data.variables {3}.dimensions = [1 2];
-sample_data.variables {4}.dimensions = [1];
-sample_data.variables {5}.dimensions = [1];
-sample_data.variables {6}.dimensions = [1];
-sample_data.variables {7}.dimensions = [1];
-sample_data.variables {8}.dimensions = [1];
-sample_data.variables {9}.dimensions = [1];
+sample_data.variables {1} .dimensions = [1 2];
+sample_data.variables {2} .dimensions = [1 2];
+sample_data.variables {3} .dimensions = [1 2];
+sample_data.variables {4} .dimensions = [1 2];
+sample_data.variables {5} .dimensions = [1 2];
+sample_data.variables {6} .dimensions = [1 2];
+sample_data.variables {7} .dimensions = [1];
+sample_data.variables {8} .dimensions = [1];
+sample_data.variables {9} .dimensions = [1];
+sample_data.variables {10}.dimensions = [1];
+sample_data.variables {11}.dimensions = [1];
+sample_data.variables {12}.dimensions = [1];
 
-sample_data.dimensions{1}.data = time;
-sample_data.dimensions{2}.data = depth;
-sample_data.variables {1}.data = velocity1;
-sample_data.variables {2}.data = velocity2;
-sample_data.variables {3}.data = velocity3;
-sample_data.variables {4}.data = temperature;
-sample_data.variables {5}.data = pressure;
-sample_data.variables {6}.data = battery;
-sample_data.variables {7}.data = pitch;
-sample_data.variables {8}.data = roll;
-sample_data.variables {9}.data = heading;
+sample_data.dimensions{1} .data = time;
+sample_data.dimensions{2} .data = depth;
+sample_data.variables {1} .data = velocity1;
+sample_data.variables {2} .data = velocity2;
+sample_data.variables {3} .data = velocity3;
+sample_data.variables {4} .data = backscatter1;
+sample_data.variables {5} .data = backscatter2;
+sample_data.variables {6} .data = backscatter3;
+sample_data.variables {7} .data = temperature;
+sample_data.variables {8} .data = pressure;
+sample_data.variables {9} .data = battery;
+sample_data.variables {10}.data = pitch;
+sample_data.variables {11}.data = roll;
+sample_data.variables {12}.data = heading;
 
 %
 % if wave data files are present, read them in
