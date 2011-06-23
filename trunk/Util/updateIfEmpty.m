42907d5b16d8296ad65aa889d89374ce8e9f08d6
guillaume.galibert@gmail.com <guillaume.galibert@gmail.com@e4b457b6-3699-3e31-c660-623cc9554e38>
2011-06-23 23:57:48 +0000

    modified util function updateIfEmpty for use of "if" in NetCDF templates

--- trunk/Util/updateIfEmpty.m
+++ trunk/Util/updateIfEmpty.m
@@ -1,16 +1,18 @@
-function value = updateIfEmpty(oldValue, newValue)
-%UPDATEIFEMPTY Return newValue if oldValue is empty, otherwise return
-%oldValue
+function value = updateIfEmpty(testValue, emptyValue, notEmptyValue)
+%UPDATEIFEMPTY Return emptyValue if testValue is empty, otherwise return
+%testValue or notEmptyValue if defined. Useful in template file to perform
+%some IF statements
 %
 %
 % Inputs:
 %
-%   oldValue    - the line to parse.
-%   newValue    - A single struct containing sample data. 
+%   testValue       - object checked if empty
+%   emptyValue      - object to be returned if testValue is empty
+%   notEmptyValue   - object to be returned if testValue is not empty
 %
 % Outputs:
 %
-%   value       - String containing the attribute value.
+%   value           - object returned.
 %
 % Author: Guillaume Galibert <guillaume.galibert@utas.edu.au>
 %
@@ -44,9 +46,13 @@ function value = updateIfEmpty(oldValue, newValue)
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 % POSSIBILITY OF SUCH DAMAGE.
 %
-  if isempty(oldValue)
-      value = newValue;
+  error(nargchk(2,3,nargin));
+
+  if nargin == 2, notEmptyValue = testValue; end
+  
+  if isempty(testValue)
+      value = emptyValue;
   else
-      value = oldValue;
+      value = notEmptyValue;
   end
 end
