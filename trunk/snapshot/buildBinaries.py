6fc22ea27b43149badd70f78436c6c792ed85c6d
guillaume.galibert@gmail.com <guillaume.galibert@gmail.com@e4b457b6-3699-3e31-c660-623cc9554e38>
2013-07-26 07:55:53 +0000

    -Binaries should now be built and commited to SVN manually. Use buildBinaries.py. -Snapshot.py no longer builds binaries but export them from SVN to package them and uplaod to google code.

---
+++ trunk/snapshot/buildBinaries.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import os
+import sys
+import time
+import shutil
+import ziputil
+import gmail
+
+# buildBinaries.py
+# Exports the IMOS Toolbox from SVN and
+#
+#   - Runs Util/imosCompile.m to create a ddb.jar and imosToolbox executables
+# 
+# Both of these files are copied to the relevant directory and commited to SVN.
+#
+# python, svn (SlikSvn), javac, ant and matlab must be on PATH
+# JAVA_HOME must be set
+#
+
+lt = time.localtime()
+
+project = 'imos-toolbox'
+
+url        = 'http://%s.googlecode.com/svn/trunk' % project
+exportDir  = 'export'
+
+compilerLog = '.\%s\log.txt' % exportDir
+
+#
+# export from SVN
+#
+print('\n--exporting tree from %s to %s' % (url, exportDir))
+os.system('svn export %s %s' % (url, exportDir))
+
+#
+# remove Tests and snapshot directories
+#
+print('\n--removing Tests and snapshot')
+shutil.rmtree('%s/Tests' % exportDir)
+shutil.rmtree('%s/snapshot' % exportDir)
+
+#
+# build DDB interface
+#
+print('\n--building DDB interface')
+compiled = os.system('cd %s/Java && ant install' % exportDir)
+
+if compiled is not 0:
+  print('\n--DDB interface compilation failed - cleaning')
+  os.system('cd %s/Java && ant clean' % exportDir)
+  gmail.send(
+    "guillaume.galibert@utas.edu.au",
+    "[imos-toolbox] DDB interface compilation error",
+    "Check the Java DDB interface code",
+    None,
+    "guillaume.galibert",
+    "28gg=!bb")
+
+#
+# create snapshot
+#
+print('\n--creating snapshot')
+matlabOpts = '-wait -nosplash -nodesktop -logfile "%s"' % compilerLog
+matlabCmd = "addpath('Util'); try, imosCompile(); exit(); catch e, disp(e.message); end;"
+os.system('cd %s && matlab %s -r "%s"' % (exportDir, matlabOpts, matlabCmd))
+
+print('\n--removing local SVN tree')
+shutil.rmtree('%s' % exportDir)
