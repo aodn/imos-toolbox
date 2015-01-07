8cb1597791f0077a5c70874e56c208375b37a6e2
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-01-07 00:24:56 +0000

    -imosTeledyneSetQC has been rename teledyneSetQC and is now replaced by a set of QC tests : *imosHorizontalVelocitySetQC *imosVerticalVelocityQC *imosErrorVelocitySetQC *imosPercentGoodVelocitySetQC *imosCorrMagVelocitySetQC *imosEchoIntensityVelocitySetQC *imosTier2ProfileVelocitySetQC Test against previous imosTeledyneSetQC test has been performed to make sure results are consistents.
    -imosSideLobeContSetQC test has ben renamed imosSideLobeVelocitySetQC for consistency with other velocity tests.
    -imosTiltSetQC test has ben renamed imosTiltVelocitySetQC for consistency with other velocity tests.
    -default correlation magnitude threshold for teledyneSetQC and imosCorrMagVelocitySetQC has now been set to 64 instead of 110.
    -toolboxProperties has been updated accordingly.

---
+++ AutomaticQC/imosCorrMagVelocitySetQC.m
@@ -0,0 +1,149 @@
+function [sample_data, varChecked, paramsLog] = imosCorrMagVelocitySetQC( sample_data, auto )
+%IMOSCORRMAGVELOCITYSETQC Quality control procedure for Teledyne Workhorse (and similar)
+% ADCP instrument data, using the correlation magnitude velocity diagnostic variable.
+%
+%
+% Inputs:
+%   sample_data - struct containing the entire data set and dimension data.
+%   auto - logical, run QC in batch mode
+%
+% Outputs:
+%   sample_data - same as input, with QC flags added for variable/dimension
+%                 data.
+%   varChecked  - cell array of variables' name which have been checked
+%   paramsLog   - string containing details about params' procedure to include in QC log
+%
+% Author:       Guillaume Galibert <guillaume.galibert@utas.edu.au>
+%
+
+%
+% Copyright (c) 2009, eMarine Information Infrastructure (eMII) and Integrated
+% Marine Observing System (IMOS).
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+%     * Redistributions of source code must retain the above copyright notice,
+%       this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in the
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of the eMII/IMOS nor the names of its contributors
+%       may be used to endorse or promote products derived from this software
+%       without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+error(nargchk(1, 2, nargin));
+if ~isstruct(sample_data), error('sample_data must be a struct'); end
+
+% auto logical in input to enable running under batch processing
+if nargin<2, auto=false; end
+
+varChecked = {};
+paramsLog  = [];
+
+% get all necessary dimensions and variables id in sample_data struct
+idUcur = 0;
+idVcur = 0;
+idWcur = 0;
+idCspd = 0;
+idCdir = 0;
+idCMAG = cell(4, 1);
+for j=1:4
+    idCMAG{j}  = 0;
+end
+lenVar = size(sample_data.variables, 2);
+for i=1:lenVar
+    paramName = sample_data.variables{i}.name;
+    
+    if strncmpi(paramName, 'UCUR', 4),  idUcur = i; end
+    if strncmpi(paramName, 'VCUR', 4),  idVcur = i; end
+    if strcmpi(paramName, 'WCUR'),      idWcur = i; end
+    if strcmpi(paramName, 'CSPD'),      idCspd = i; end
+    if strncmpi(paramName, 'CDIR', 4),  idCdir = i; end
+    for j=1:4
+        cc = int2str(j);
+        if strcmpi(paramName, ['CMAG' cc]), idCMAG{j} = i; end
+    end
+end
+
+% check if the data is compatible with the QC algorithm
+idMandatory = (idUcur | idVcur | idWcur | idCspd | idCdir);
+for j=1:4
+    idMandatory = idMandatory & idCMAG{j};
+end
+if ~idMandatory, return; end
+
+qcSet           = str2double(readProperty('toolbox.qc_set'));
+badFlag         = imosQCFlag('bad',             qcSet, 'flag');
+goodFlag        = imosQCFlag('good',            qcSet, 'flag');
+rawFlag         = imosQCFlag('raw',             qcSet, 'flag');
+
+%Pull out correlation magnitude
+sizeData = size(sample_data.variables{idCMAG{1}}.data);
+cr = nan(4, sizeData(1), sizeData(2));
+for j=1:4;
+    cr(j, :, :) = sample_data.variables{idCMAG{j}}.data;
+end
+
+% read in filter parameters
+propFile = fullfile('AutomaticQC', 'imosCorrMagVelocitySetQC.txt');
+cmag     = str2double(readProperty('cmag',   propFile));
+
+paramsLog = ['cmag=' num2str(cmag)];
+
+sizeCur = size(sample_data.variables{idUcur}.flags);
+
+% same flags are given to any variable
+flags = ones(sizeCur, 'int8')*rawFlag;
+
+% Run QC
+isub1 = (cr(1, :, :) > cmag);
+isub2 = (cr(2, :, :) > cmag);
+isub3 = (cr(3, :, :) > cmag);
+isub4 = (cr(4, :, :) > cmag);
+% test nbins bins
+isub_all = isub1+isub2+isub3+isub4;
+clear isub1 isub2 isub3 isub4;
+
+% assign pass(1) or fail(0) values
+% Where 2 or more beams pass, then the cmag test is passed
+iPass = isub_all >= 2;
+iFail = ~iPass;
+clear isub_all;
+
+% Run QC filter (iFail) on velocity data
+flags(iFail) = badFlag;
+flags(iPass) = goodFlag;
+
+sample_data.variables{idUcur}.flags = flags;
+sample_data.variables{idVcur}.flags = flags;
+sample_data.variables{idWcur}.flags = flags;
+
+varChecked = {sample_data.variables{idUcur}.name, ...
+    sample_data.variables{idVcur}.name, ...
+    sample_data.variables{idWcur}.name};
+
+if idCdir
+    sample_data.variables{idCdir}.flags = flags;
+    varChecked = [varChecked, {sample_data.variables{idCdir}.name}];
+end
+
+if idCspd
+    sample_data.variables{idCspd}.flags = flags;
+    varChecked = [varChecked, {sample_data.variables{idCspd}.name}];
+end
+    
+end
---
+++ AutomaticQC/imosVerticalVelocityQC.m
@@ -0,0 +1,109 @@
+function [data, flags, paramsLog] = imosVerticalVelocityQC ( sample_data, data, k, type, auto )
+%IMOSVERTICALVELOCITYQC Quality control procedure for vertical velocity of 
+% any ADCP instrument data.
+%
+%
+% Inputs:
+%   sample_data - struct containing the entire data set and dimension data.
+%
+%   data        - the vector/matrix of data to check.
+%
+%   k           - Index into the sample_data.variables vector.
+%
+%   type        - dimensions/variables type to check in sample_data.
+%
+%   auto        - logical, run QC in batch mode
+%
+% Outputs:
+%   data        - same as input.
+%
+%   flags       - Vector the same length as data, with flags for corresponding
+%                 data which is out of range.
+%
+%   paramsLog   - string containing details about params' procedure to include in QC log
+%
+% Author:       Guillaume Galibert <guillaume.galibert@utas.edu.au>
+%
+
+%
+% Copyright (c) 2009, eMarine Information Infrastructure (eMII) and Integrated 
+% Marine Observing System (IMOS).
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are met:
+% 
+%     * Redistributions of source code must retain the above copyright notice, 
+%       this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in the 
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of the eMII/IMOS nor the names of its contributors 
+%       may be used to endorse or promote products derived from this software 
+%       without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
+%
+
+error(nargchk(4, 5, nargin));
+if ~isstruct(sample_data),              error('sample_data must be a struct');      end
+if ~isscalar(k) || ~isnumeric(k),       error('k must be a numeric scalar');        end
+if ~ischar(type),                       error('type must be a string');             end
+
+% auto logical in input to enable running under batch processing
+if nargin<5, auto=false; end
+
+paramsLog = [];
+flags     = [];
+
+% this test doesn't apply on dimensions nor LATITUDE, LONGITUDE, nor NOMINAL_DEPTH variables
+if ~strcmp(type, 'variables'), return; end
+if any(strcmp(sample_data.(type){k}.name, {'LATITUDE', 'LONGITUDE', 'NOMINAL_DEPTH'})), return; end
+
+% get all necessary dimensions and variables id in sample_data struct
+idWcur = 0;
+paramName = sample_data.(type){k}.name;
+if strcmpi(paramName, 'WCUR'), idWcur = k; end
+
+% check if the data is compatible with the QC algorithm
+idMandatory = idWcur;
+if ~idMandatory, return; end
+
+qcSet           = str2double(readProperty('toolbox.qc_set'));
+badFlag         = imosQCFlag('bad',             qcSet, 'flag');
+goodFlag        = imosQCFlag('good',            qcSet, 'flag');
+rawFlag         = imosQCFlag('raw',             qcSet, 'flag');
+
+%Pull out vertical velocities
+w = sample_data.variables{idWcur}.data;
+
+% read in filter parameters
+propFile = fullfile('AutomaticQC', 'imosVerticalVelocityQC.txt');
+vvel      = str2double(readProperty('vvel',      propFile));
+
+paramsLog = ['vvel=' num2str(vvel)];
+
+sizeCur = size(sample_data.variables{idWcur}.flags);
+
+% same flags are given to any variable
+flags = ones(sizeCur, 'int8')*rawFlag;
+
+%Run QC
+iPass = abs(w) <= vvel;
+iFail = ~iPass;
+
+%Run QC filter (iFail) on velocity data
+flags(iFail) = badFlag;
+flags(iPass) = goodFlag;
+
+end
