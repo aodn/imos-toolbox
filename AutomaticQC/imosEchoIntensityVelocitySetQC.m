20fffa5b22dd0280b5e3b988e31d1c04ed9a16ae
guillaume.galibert <guillaume.galibert@gmail.com>
2015-01-07 00:24:56 +0000

    -imosTeledyneSetQC has been rename teledyneSetQC and is now replaced by a set of QC tests : *imosHorizontalVelocitySetQC *imosVerticalVelocityQC *imosErrorVelocitySetQC *imosPercentGoodVelocitySetQC *imosCorrMagVelocitySetQC *imosEchoIntensityVelocitySetQC *imosTier2ProfileVelocitySetQC Test against previous imosTeledyneSetQC test has been performed to make sure results are consistents.
    -imosSideLobeContSetQC test has ben renamed imosSideLobeVelocitySetQC for consistency with other velocity tests.
    -imosTiltSetQC test has ben renamed imosTiltVelocitySetQC for consistency with other velocity tests.
    -default correlation magnitude threshold for teledyneSetQC and imosCorrMagVelocitySetQC has now been set to 64 instead of 110.
    -toolboxProperties has been updated accordingly.

---
+++ AutomaticQC/imosEchoIntensityVelocitySetQC.m
@@ -0,0 +1,177 @@
+function [sample_data, varChecked, paramsLog] = imosEchoIntensityVelocitySetQC( sample_data, auto )
+%IMOSECHOINTENSITYVELOCITYSETQC Quality control procedure for Teledyne Workhorse (and similar)
+% ADCP instrument data, using the echo intensity velocity diagnostic variable.
+%
+% Echo Amplitude test :
+% this test looks at the difference between consecutive vertical bin values of ea and
+% if the value exceeds the threshold, then the bin fails, and all bins
+% above it are also considered to have failed. This test is designed to get 
+% rid of above/below surface/bottom bins.
+%
+% Inputs:
+%   sample_data - struct containing the entire data set and dimension data.
+%   auto - logical, run QC in batch mode
+%
+% Outputs:
+%   sample_data - same as input, with QC flags added for variable/dimension
+%                 data.
+%   varChecked  - cell array of variables' name which have been checked
+%   paramsLog   - string containing details about params' procedure to include in QC log
+%
+% Author:       Guillaume Galibert <guillaume.galibert@utas.edu.au>
+%
+
+%
+% Copyright (c) 2009, eMarine Information Infrastructure (eMII) and Integrated
+% Marine Observing System (IMOS).
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+%     * Redistributions of source code must retain the above copyright notice,
+%       this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in the
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of the eMII/IMOS nor the names of its contributors
+%       may be used to endorse or promote products derived from this software
+%       without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+error(nargchk(1, 2, nargin));
+if ~isstruct(sample_data), error('sample_data must be a struct'); end
+
+% auto logical in input to enable running under batch processing
+if nargin<2, auto=false; end
+
+varChecked = {};
+paramsLog  = [];
+
+% get all necessary dimensions and variables id in sample_data struct
+idUcur = 0;
+idVcur = 0;
+idWcur = 0;
+idCspd = 0;
+idCdir = 0;
+idABSI = cell(4, 1);
+for j=1:4
+    idABSI{j}  = 0;
+end
+lenVar = size(sample_data.variables, 2);
+for i=1:lenVar
+    paramName = sample_data.variables{i}.name;
+    
+    if strncmpi(paramName, 'UCUR', 4),  idUcur = i; end
+    if strncmpi(paramName, 'VCUR', 4),  idVcur = i; end
+    if strcmpi(paramName, 'WCUR'),      idWcur = i; end
+    if strcmpi(paramName, 'CSPD'),      idCspd = i; end
+    if strncmpi(paramName, 'CDIR', 4),  idCdir = i; end
+    for j=1:4
+        cc = int2str(j);
+        if strcmpi(paramName, ['ABSI' cc]), idABSI{j} = i; end
+    end
+end
+
+% check if the data is compatible with the QC algorithm
+idMandatory = (idUcur | idVcur | idWcur | idCspd | idCdir);
+for j=1:4
+    idMandatory = idMandatory & idABSI{j};
+end
+if ~idMandatory, return; end
+
+qcSet           = str2double(readProperty('toolbox.qc_set'));
+badFlag         = imosQCFlag('bad',             qcSet, 'flag');
+goodFlag        = imosQCFlag('good',            qcSet, 'flag');
+rawFlag         = imosQCFlag('raw',             qcSet, 'flag');
+
+%Pull out echo intensity
+sizeData = size(sample_data.variables{idABSI{1}}.data);
+ea = nan(4, sizeData(1), sizeData(2));
+for j=1:4;
+    ea(j, :, :) = sample_data.variables{idABSI{j}}.data;
+end
+
+% read in filter parameters
+propFile  = fullfile('AutomaticQC', 'imosEchoIntensityVelocitySetQC.txt');
+ea_thresh = str2double(readProperty('ea_thresh',   propFile));
+
+paramsLog = ['ea_thresh=' num2str(ea_thresh)];
+
+sizeCur = size(sample_data.variables{idUcur}.flags);
+
+% same flags are given to any variable
+flags = ones(sizeCur, 'int8')*rawFlag;
+
+% Run QC
+% this test looks at the difference between consecutive vertical bin values of ea and
+% if the value exceeds the threshold, then the bin fails, and all bins
+% above it are also considered to have failed. This test is designed to get 
+% rid of above/below surface/bottom bins.
+lenTime = sizeCur(1);
+lenBin  = sizeCur(2);
+
+% if the following test is successfull, the bin gets good
+ib = uint8(diff(squeeze(ea(1, :,:)),1,2) <= ea_thresh) + ...
+     uint8(diff(squeeze(ea(2, :,:)),1,2) <= ea_thresh) + ...
+     uint8(diff(squeeze(ea(3, :,:)),1,2) <= ea_thresh) + ...
+     uint8(diff(squeeze(ea(4, :,:)),1,2) <= ea_thresh);
+
+% we look for the bins that have 3 or more beams that pass the tests
+ib = ib >= 3;
+ 
+% we assume the first bin is good
+ib = [true(lenTime, 1), ib];
+ 
+% any good bin further than a bad one should be set bad
+jkf = repmat(single(1:1:lenBin), [lenTime, 1]);
+
+iii = single(~ib).*jkf;
+clear ib;
+iii(iii == 0) = NaN;
+iif = min(iii, [], 2);
+clear iii;
+iifNotNan = ~isnan(iif);
+
+iPass = true(lenTime, lenBin);
+if any(iifNotNan)
+    % all bins further than the first bad one is reset to bad
+    iPass(jkf >= repmat(iif, [1, lenBin])) = false;
+end
+iFail = ~iPass;
+clear iifNotNan iif jkf;
+
+% Run QC filter (iFail) on velocity data
+flags(iFail) = badFlag;
+flags(iPass) = goodFlag;
+
+sample_data.variables{idUcur}.flags = flags;
+sample_data.variables{idVcur}.flags = flags;
+sample_data.variables{idWcur}.flags = flags;
+
+varChecked = {sample_data.variables{idUcur}.name, ...
+    sample_data.variables{idVcur}.name, ...
+    sample_data.variables{idWcur}.name};
+
+if idCdir
+    sample_data.variables{idCdir}.flags = flags;
+    varChecked = [varChecked, {sample_data.variables{idCdir}.name}];
+end
+
+if idCspd
+    sample_data.variables{idCspd}.flags = flags;
+    varChecked = [varChecked, {sample_data.variables{idCspd}.name}];
+end
+    
+end
---
+++ AutomaticQC/imosHorizontalVelocitySetQC.m
@@ -0,0 +1,124 @@
+function [sample_data, varChecked, paramsLog] = imosHorizontalVelocitySetQC( sample_data, auto )
+%IMOSHORIZONTALVELOCITYSETQC Quality control procedure for horizontal
+% velocity of any ADCP instrument data.
+%
+%
+% Inputs:
+%   sample_data - struct containing the entire data set and dimension data.
+%   auto - logical, run QC in batch mode
+%
+% Outputs:
+%   sample_data - same as input, with QC flags added for variable/dimension
+%                 data.
+%   varChecked  - cell array of variables' name which have been checked
+%   paramsLog   - string containing details about params' procedure to include in QC log
+%
+% Author:       Guillaume Galibert <guillaume.galibert@utas.edu.au>
+%
+
+%
+% Copyright (c) 2009, eMarine Information Infrastructure (eMII) and Integrated
+% Marine Observing System (IMOS).
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+%     * Redistributions of source code must retain the above copyright notice,
+%       this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in the
+%       documentation and/or other materials provided with the distribution.
+%     * Neither the name of the eMII/IMOS nor the names of its contributors
+%       may be used to endorse or promote products derived from this software
+%       without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+error(nargchk(1, 2, nargin));
+if ~isstruct(sample_data), error('sample_data must be a struct'); end
+
+% auto logical in input to enable running under batch processing
+if nargin<2, auto=false; end
+
+varChecked = {};
+paramsLog  = [];
+
+% get all necessary dimensions and variables id in sample_data struct
+idUcur = 0;
+idVcur = 0;
+idCspd = 0;
+idCdir = 0;
+lenVar = size(sample_data.variables, 2);
+for i=1:lenVar
+    paramName = sample_data.variables{i}.name;
+    
+    if strncmpi(paramName, 'UCUR', 4),  idUcur = i; end
+    if strncmpi(paramName, 'VCUR', 4),  idVcur = i; end
+    if strcmpi(paramName, 'CSPD'),      idCspd = i; end
+    if strncmpi(paramName, 'CDIR', 4),  idCdir = i; end
+end
+
+% check if the data is compatible with the QC algorithm
+idMandatory = idUcur & idVcur;
+if ~idMandatory, return; end
+
+qcSet           = str2double(readProperty('toolbox.qc_set'));
+badFlag         = imosQCFlag('bad',             qcSet, 'flag');
+goodFlag        = imosQCFlag('good',            qcSet, 'flag');
+rawFlag         = imosQCFlag('raw',             qcSet, 'flag');
+
+% Pull out horizontal velocities
+% we can afford to run the test only once (if couple of UCUR/VCUR) since u
+% is only tested in absolute value (direction doesn't matter)
+u = sample_data.variables{idUcur}.data;
+v = sample_data.variables{idVcur}.data;
+velocity = u + 1i*v;
+clear u v;
+
+% read in filter parameters
+propFile = fullfile('AutomaticQC', 'imosHorizontalVelocitySetQC.txt');
+hvel     = str2double(readProperty('hvel',      propFile));
+
+paramsLog = ['hvel=' num2str(hvel)];
+
+sizeCur = size(sample_data.variables{idUcur}.flags);
+
+% same flags are given to any variable
+flags = ones(sizeCur, 'int8')*rawFlag;
+
+% Run QC
+iPass = abs(velocity) <= hvel;
+iFail = ~iPass;
+
+% Run QC filter (iFail) on velocity data
+flags(iFail) = badFlag;
+flags(iPass) = goodFlag;
+
+sample_data.variables{idUcur}.flags = flags;
+sample_data.variables{idVcur}.flags = flags;
+
+varChecked = {sample_data.variables{idUcur}.name, ...
+    sample_data.variables{idVcur}.name};
+
+if idCdir
+    sample_data.variables{idCdir}.flags = flags;
+    varChecked = [varChecked, {sample_data.variables{idCdir}.name}];
+end
+
+if idCspd
+    sample_data.variables{idCspd}.flags = flags;
+    varChecked = [varChecked, {sample_data.variables{idCspd}.name}];
+end
+    
+end
