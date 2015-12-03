d1abf8ee5e6174fcd17772699769eebada420791
Peter Jansen <peter.jansen@csiro.au>
2015-12-03 18:39:03 +1100

    Re-format Code
    Clean code formatting

--- Java/src/org/imos/ddb/CryptCodecOpener.java
+++ Java/src/org/imos/ddb/CryptCodecOpener.java
@@ -9,16 +9,20 @@ import com.healthmarketscience.jackcess.CryptCodecProvider;
 import com.healthmarketscience.jackcess.Database;
 import com.healthmarketscience.jackcess.DatabaseBuilder;
 
-public class CryptCodecOpener implements JackcessOpenerInterface {
-  public Database open(File fl,String pwd) throws IOException {
-   DatabaseBuilder dbd =new DatabaseBuilder(fl);
-   dbd.setAutoSync(true);
-   dbd.setCodecProvider(new CryptCodecProvider(pwd));
-   dbd.setReadOnly(false);
-   return dbd.open();
-   
-  }
-  //Notice that the parameter setting autosync =true is recommended with UCanAccess for performance reasons.
-  //UCanAccess flushes the updates to disk at transaction end.
-  //For more details about autosync parameter (and related tradeoff), see the Jackcess documentation.
+public class CryptCodecOpener implements JackcessOpenerInterface
+{
+	public Database open(File fl, String pwd) throws IOException
+	{
+		DatabaseBuilder dbd = new DatabaseBuilder(fl);
+		dbd.setAutoSync(true);
+		dbd.setCodecProvider(new CryptCodecProvider(pwd));
+		dbd.setReadOnly(false);
+		return dbd.open();
+
+	}
+	// Notice that the parameter setting autosync =true is recommended with
+	// UCanAccess for performance reasons.
+	// UCanAccess flushes the updates to disk at transaction end.
+	// For more details about autosync parameter (and related tradeoff), see the
+	// Jackcess documentation.
 }
