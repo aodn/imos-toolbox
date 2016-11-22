ae925ddcada9ba05b91bb4f1e9907394c63ce377
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2016-11-22 16:57:41 +1100

    Increment version to 2.5.21 and update binaries accordingly.

--- imosToolbox.m
+++ imosToolbox.m
@@ -73,7 +73,7 @@ for j = 1 : length(jars)
 end
 
 % Set current toolbox version
-toolboxVersion = ['2.5.20 - ' computer];
+toolboxVersion = ['2.5.21 - ' computer];
 
 switch auto  
   case 'auto', autoIMOSToolbox(toolboxVersion, varargin{:});
