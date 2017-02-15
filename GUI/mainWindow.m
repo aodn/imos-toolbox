8c3c1f805692644fcc982dee1e394d3880be651f
sspagnol <s.spagnol@gmail.com>
2017-02-15 12:18:31 +0800

    Fix variable name inferred from list of ticked boxes
    UserData returned is in the form {line{k}, k} i.e. handle to a line object and an index of the plot, so previous code hTickBoxes(iTickBox) would only be the iTickBox index into all check boxes, not index into currently ticked boxes.

--- GUI/mainWindow.m
+++ GUI/mainWindow.m
@@ -499,8 +499,9 @@ function mainWindow(...
                 userData = get(graphs1D(i), 'UserData');
                 hData = userData{1};
                 iTickBox = userData{2};
+                iAllTickedBox = find(cell2mat(get(hTickBoxes,'Value'))==1);
                 
-                varName = get(hTickBoxes(iTickBox), 'String');
+                varName = get(hTickBoxes(iAllTickedBox(iTickBox)), 'String');
                 iVar = getVar(sam.variables, varName);
                 flags = sam.variables{iVar}.flags;
                 iGood = ismember(flags, okFlags);
