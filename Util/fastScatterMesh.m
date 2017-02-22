87866cf729018149a7515186604e5da4203d83a1
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2017-02-22 15:46:31 +1100

    Fix fastScatterMesh.m, zbuffer wrongly defined which can cause errors.

--- Util/fastScatterMesh.m
+++ Util/fastScatterMesh.m
@@ -20,25 +20,16 @@ nColors = length(cmap);
 
 minClim = colLimits(1);
 maxClim = colLimits(2);
-    
-% Posting by Boris Babic for the method. see http://www.mathworks.com/matlabcentral/newsreader/view_thread/22966
-% as referenced in
-% http://au.mathworks.com/matlabcentral/fileexchange/47205-fastscatter-m
-% http://au.mathworks.com/matlabcentral/fileexchange/53580-fastscatterm
 
 % index of valid data
-ix=find(isfinite(cdata + xdata + ydata));
-
-if isempty(ix) | numel(ix) < 2
-    hwin = [];
+ix = find(isfinite(cdata + xdata + ydata));
+if isempty(ix) || numel(ix) < 2
     return;
 end
 
-if mod(length(ix),2)==1
-    ix(end+1)=ix(end);
+if mod(length(ix), 2) == 1
+    ix(end+1) = ix(end);
 end
-
-ixLength = length(ix);
 
 % normalize cdata
 normData = (cdata(ix) - minClim) / ( maxClim - minClim );
@@ -55,26 +46,26 @@ end
 
 switch zType
     case 'ascending',
-        zBuffer = linspace(0, 1, ixLength);
+        zBuffer = linspace(0, 1, nColors);
     case 'descending',
-        zBuffer = linspace(1, 0, ixLength) ;
+        zBuffer = linspace(1, 0, nColors) ;
     case 'triangle',
-        zBuffer = [linspace(0, 1, ixLength/2) linspace(1, 0, ixLength/2)];
+        zBuffer = [linspace(0, 1, nColors/2) linspace(1, 0, nColors/2)];
     case 'vee',
-        zBuffer = [linspace(1, 0, ixLength/2) linspace(0, 1, ixLength/2)];
+        zBuffer = [linspace(1, 0, nColors/2) linspace(0, 1, nColors/2)];
     case 'flat',
         zBuffer = zeros(size(ix));
     case 'parabolic'
-        jj = (0:ixLength-1)';
-        zBuffer = 1 - (2*jj/ixLength - 1).^2;
+        jj = (0:nColors-1)';
+        zBuffer = 1 - (2*jj/nColors - 1).^2;
     case 'hamming'
-        thalf=pi/ixLength*((1-ixLength):2:0)';         % first half sample locations
+        thalf=pi/nColors*((1-nColors):2:0)';         % first half sample locations
         hwin=.54+.46*cos(thalf);                       % first half window
-        zBuffer=[hwin; hwin(floor(ixLength/2):-1:1)];  % full window
+        zBuffer=[hwin; hwin(floor(nColors/2):-1:1)];  % full window
     case 'hann'
-        thalf=pi/ixLength*((1-ixLength):2:0)';         % first half sample locations
+        thalf=pi/nColors*((1-nColors):2:0)';         % first half sample locations
         hwin=.5+.5*cos(thalf);                         % first half window
-        zBuffer=[hwin; hwin(floor(ixLength/2):-1:1)];  % full window
+        zBuffer=[hwin; hwin(floor(nColors/2):-1:1)];  % full window
     otherwise
         warning('Unknown marker zbuffer method, using flat');
         zBuffer = zeros(size(ix));
