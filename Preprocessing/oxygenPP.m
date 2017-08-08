11b157c02ceed51b2764b8a059cb071260b354b8
Peter Jansen <peter.jansen@csiro.au>
2017-08-08 13:16:55 +1000

    calculate presRel for calcs from avaliable pressure/depth
    move calculation of presRel in salinityPP.m into a separate function,
    and use to get presRel for oxygen DOX2 calculation.

--- Preprocessing/oxygenPP.m
+++ Preprocessing/oxygenPP.m
@@ -34,6 +34,19 @@ function sample_data = oxygenPP( sample_data, qcLevel, auto )
 % Author:       Peter Jansen <peter.jansen@csiro.au>
 %
 
+% From : http://doi.org/10.13155/39795
+%
+% The unit of DOXY is umol/kg in Argo data and the oxygen measurements are sent from Argo floats in 
+% another unit such as umol/L for the Optode and ml/L for the SBE-IDO. Thus the unit conversion is carried out by DACs as follows:
+%   O2 [umol/L] = 44.6596 . O2 [ml/L]
+%   O2 [umol/kg] = O2 [umol/L] / pdens
+% Here, pdens is the potential density of water [kg/L] at zero pressure and at the potential temperature 
+% (e.g., 1.0269 kg/L; e.g., UNESCO, 1983). The value of 44.6596 is derived from the molar volume of the oxygen gas, 22.3916 L/mole, 
+% at standard temperature and pressure (0 C, 1 atmosphere; e.g., Garcia and Gordon, 1992). 
+% This unit conversion follows the "Recommendations on the conversion between oxygen quantities for Bio-Argo floats and other autonomous sensor platforms" 
+% by SCOR Working Group 142 on "Quality Control Procedures for Oxygen and Other Biogeochemical Sensors on Floats and Gliders" [RD13] . 
+% The unit conversion should always be done with the best available temperature, i.e., TEMP of the CTD unit.
+
 %
 % Copyright (c) 2016, Australian Ocean Data Network (AODN) and Integrated 
 % Marine Observing System (IMOS).
@@ -94,16 +107,9 @@ for k = 1:length(sample_data)
   
   % indexs of other variables that might be needed
   psalIdx       = getVar(sam.variables, 'PSAL');
-  presRelIdx    = getVar(sam.variables, 'PRES_REL');
-  presIdx       = getVar(sam.variables, 'PRES');
   
-  if (presRelIdx)
-      pres = sam.variables{presRelIdx}.data;
-  else
-      % Should be pressure, convert from depth?
-      pres = sam.instrument_nominal_depth*ones(size(temp));
-  end
-  
+  [presRel, presName] = presRefFromSampleData(sam);
+    
   % oxygen parameters that maybe in the dataset
   doxIdx        = getVar(sam.variables, 'DOX');
   dox1Idx       = getVar(sam.variables, 'DOX1');
@@ -113,31 +119,10 @@ for k = 1:length(sample_data)
   
   % check is psal and some dox measurement present, if not skip
   if ~(psalIdx && (doxIdx || dox1Idx || dox2Idx || doxsIdx)), continue; end
-  
-%   % get a latitude for calculation of Salinity Absolute from TEOS-10
-%   if isempty(sam.geospatial_lat_min)
-%       disp(['Warning : no geospatial_lat_min, geospatial_long_min documented for oxygenPP to be applied on ' sam.toolbox_input_file]);
-%       prompt = {'Latitude (South -ve)', 'Longitude (West -ve)'};
-%       dlg_title = 'Location';
-%       num_lines = 1;
-%       defaultans = {'0' , '0'};
-%       answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
-%       
-%       % don't try to apply any correction if canceled by user
-%       if isempty(answer), return; end
-%       
-%       sam.geospatial_lat_min = str2double(answer(2));
-%       continue;
-%   end
-%   lat = sam.geospatial_lat_min;
-%   long = sam.geospatial_long_min;
-  
+    
   psal = sam.variables{psalIdx}.data;
   
-  ptmp = sw_ptmp(psal, temp , pres, 0);
-  
-  %SA = gsw_SA_from_SP(psal, pres, long, lat);
-  %pt = gsw_pt_from_t(SA, temp, pres, 0);
+  ptmp = sw_ptmp(psal, temp , presRel, 0);
   
   % calculate the density at 0 dbar
   dens = sw_dens0(psal, ptmp);
@@ -152,13 +137,13 @@ for k = 1:length(sample_data)
     
   if (doxIdx) && ~(dox2Idx)
     dox = sam.variables{doxIdx}.data;
-    dox2 = dox * 44660 ./ dens;
-    dox2Comment = 'oxsolPP.m: derived from dox2 = dox (ml/l) * 44660 / sw_dens0(PSAL,TEMP)';
+    dox2 = dox * 44659.6 ./ dens;
+    dox2Comment = 'oxsolPP.m: derived from dox2 = dox (ml/l) * 44659.6 / sw_dens0(PSAL, PTEMP)';
   end
   if (dox1Idx) && ~(dox2Idx)
     dox1 = sam.variables{dox1Idx}.data;
     dox2 = dox1 ./ dens;
-    dox2Comment = 'oxsolPP.m: derived from dox2 = dox (umol/l) / sw_dens0(PSAL,TEMP)';
+    dox2Comment = 'oxsolPP.m: derived from dox2 = dox (umol/l) / sw_dens0(PSAL, PTEMP)';
   end
   if (doxsIdx) && ~(dox2Idx)
     doxs = sam.variables{doxsIdx}.data;
@@ -172,8 +157,11 @@ for k = 1:length(sample_data)
       sam.variables{end}.typeCastFunc = str2func(netcdf3ToMatlabType(imosParameters(sam.variables{end}.name, 'type')));
       sam.variables{end}.coordinates  = coordinates;
   
-      sam.variables{end}.data = sam.variables{end}.typeCastFunc(dox2);
-      sam.variables{end}.comment       = dox2Comment;
+      sam.variables{end}.data                   = sam.variables{end}.typeCastFunc(dox2);
+      sam.variables{end}.comment                = dox2Comment;
+      sam.variables{end}.conversion_reference   = 'http://doi.org/10.13155/39795';
+      sam.variables{end}.comment_pressure       = ['oxygenPP.m: pressure from ' presName];
+
   else
       dox2 = sam.variables{dox2Idx}.data;
   end
@@ -201,48 +189,12 @@ for k = 1:length(sample_data)
 
   history = sam.history;
   if isempty(history)
-      sam.history = sprintf('%s - %s', datestr(now_utc, readProperty('exportNetCDF.dateFormat')), oxsolComment);
+      sam.history = sprintf('%s - %s', datestr(now_utc, readProperty('exportNetCDF.dateFormat')), dox2Comment);
   else
-      sam.history = sprintf('%s\n%s - %s', history, datestr(now_utc, readProperty('exportNetCDF.dateFormat')), oxsolComment);
+      sam.history = sprintf('%s\n%s - %s', history, datestr(now_utc, readProperty('exportNetCDF.dateFormat')), dox2Comment);
   end
+  sam.history = sprintf('%s\n%s - %s', history, datestr(now_utc, readProperty('exportNetCDF.dateFormat')), oxsolComment);
   
   sample_data{k} = sam;
 end
 
-% FROM WQM
-%     % some fields are not in IMOS uom - scale them so that they are
-%     switch upper(fields{k})
-%         
-%         % WQM provides conductivity S/m; exactly like we want it to be!
-%         
-%         % WQM can provide Dissolved Oxygen in mmol/m3,
-%         % hopefully 1 mmol/m3 = 1 umol/l
-%         % exactly like we want it to be!
-%         case upper('DO(mmol/m^3)') % DOX1_1
-%             comment = 'Originally expressed in mmol/m3, 1l = 0.001m3 was assumed.';
-%             isUmolPerL = true;
-%             
-%         % convert dissolved oxygen in ml/l to umol/l
-%         case upper('DO(ml/l)') % DOX1_2
-%             comment = 'Originally expressed in ml/l, 1ml/l = 44.660umol/l was assumed.';
-%             isUmolPerL = true;
-%             
-%             % ml/l -> umol/l
-%             %
-%             % Conversion factors from Saunders (1986) :
-%             % https://darchive.mblwhoilibrary.org/bitstream/handle/1912/68/WHOI-89-23.pdf?sequence=3
-%             % 1ml/l = 43.57 umol/kg (with dens = 1.025 kg/l)
-%             % 1ml/l = 44.660 umol/l
-%             
-%             data = data .* 44.660;
-%             
-%         % convert dissolved oxygen in mg/L to umol/l.
-%         case upper('DO(mg/l)') % DOX1_3
-%             data = data * 44.660/1.429; % O2 density = 1.429 kg/m3
-%             comment = 'Originally expressed in mg/l, O2 density = 1.429kg/m3 and 1ml/l = 44.660umol/l were assumed.';
-%             isUmolPerL = true;
-%             
-%         % WQM provides chlorophyll in ug/L; we need it in mg/m^3, 
-%         % hopefully it is equivalent.
-%     end
-% 
