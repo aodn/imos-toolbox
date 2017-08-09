8e4af26354aff0048a9fc3bdcd22522960e10746
Peter Jansen <peter.jansen@csiro.au>
2017-08-10 09:31:48 +1000

    priority order for DOX2 caluclation, and add calc from DOXY
    Calculation order for DOX2 is now from DOXS, DOXY, DOX1, DOX.
    
    Calculate DOX2 from DOXY as dox2 = doxy (kg/m^3) * 31.9988 /
    sw_dens0(PSAL, PTEMP)

--- Preprocessing/oxygenPP.m
+++ Preprocessing/oxygenPP.m
@@ -109,12 +109,22 @@ for k = 1:length(sample_data)
   psalIdx       = getVar(sam.variables, 'PSAL');
   
   [presRel, presName] = presRelFromSampleData(sam);
-    
+
+	% from imosParameters.txt
+	% DOX,                 0, volume_concentration_of_dissolved_molecular_oxygen_in_sea_water,                          ml l-1,        ,              ,                                  O, 999999.0, 0.0,      200.0,    float
+	% DOX1,                1, mole_concentration_of_dissolved_molecular_oxygen_in_sea_water,                            umol l-1,      ,              ,                                  O, 999999.0, 0.0,      1000.0, float
+	% DOX2,                1, moles_of_oxygen_per_unit_mass_in_sea_water,                                               umol kg-1,     ,              ,                                  O, 999999.0, 0.0,      1000.0, float
+	% DOXS,                1, fractional_saturation_of_oxygen_in_sea_water,                                             percent,       ,              ,                                  O, 999999.0, ,         ,         float
+	% DOXY,                1, mass_concentration_of_oxygen_in_sea_water,                                                kg m-3,        ,              ,                                  O, 999999.0, 0.0,      29.0,     float
+	% DOXY_TEMP,           1, temperature_of_sensor_for_oxygen_in_sea_water,                                            degrees_Celsius,,             ,                                  T, 999999.0, 0.0,      50.0,     float
+	% OXSOL,               0, oxygen_solubility,                                                                        umol kg-1,     ,              ,                                  O, 999999.0, 0.0,      1000.0, float
+
   % oxygen parameters that maybe in the dataset
   doxIdx        = getVar(sam.variables, 'DOX');
   dox1Idx       = getVar(sam.variables, 'DOX1');
   dox2Idx       = getVar(sam.variables, 'DOX2');
   doxsIdx       = getVar(sam.variables, 'DOXS');
+  doxyIdx       = getVar(sam.variables, 'DOXY');
   oxsolIdx      = getVar(sam.variables, 'OXSOL');
   
   % check is psal and some dox measurement present, if not skip
@@ -134,7 +144,19 @@ for k = 1:length(sample_data)
   else
       oxsol = sam.variables{oxsolIdx}.data;
   end
-    
+
+  % derive DOX2 from DOX1, DOX, DOXY, or DOXS in that preference order DOX1 first preference, so is calculated last
+  if (doxsIdx) && ~(dox2Idx)
+    doxs = sam.variables{doxsIdx}.data;
+    dox2 = doxs .* oxsol;
+    dox2Comment = 'oxsolPP.m: derived from doxs * oxsol(PSAL, TEMP)';
+  end
+  if (doxyIdx) && ~(dox2Idx) && ~(dox1Idx)
+    doxy = sam.variables{doxyIdx}.data;
+    dox1 = doxy * 31.9988 ;
+    dox2Comment = 'oxsolPP.m: derived dox2 = doxy (kg/m^3) * 31.9988 / sw_dens0(PSAL, PTEMP)';
+    dox2 = dox1 ./ dens;
+  end
   if (doxIdx) && ~(dox2Idx)
     dox = sam.variables{doxIdx}.data;
     dox2 = dox * 44659.6 ./ dens;
@@ -144,11 +166,6 @@ for k = 1:length(sample_data)
     dox1 = sam.variables{dox1Idx}.data;
     dox2 = dox1 ./ dens;
     dox2Comment = 'oxsolPP.m: derived from dox2 = dox (umol/l) / sw_dens0(PSAL, PTEMP)';
-  end
-  if (doxsIdx) && ~(dox2Idx)
-    doxs = sam.variables{doxsIdx}.data;
-    dox2 = doxs .* oxsol;
-    dox2Comment = 'oxsolPP.m: derived from doxs * oxsol(PSAL, TEMP)';
   end
   
   if ~(dox2Idx)
