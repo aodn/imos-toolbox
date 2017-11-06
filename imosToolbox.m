bbd458016e970fdf1d4f4b9c7c17b938a29397f3
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2017-11-06 15:19:20 +1100

    Increment version to 2.5.36 and update binaries accordingly.

--- imosToolbox.m
+++ imosToolbox.m
@@ -37,7 +37,7 @@ function imosToolbox(auto, varargin)
 %
 
 % Set current toolbox version
-toolboxVersion = ['2.5.35 - ' computer];
+toolboxVersion = ['2.5.36 - ' computer];
 
 if nargin == 0, auto = 'manual'; end
 
