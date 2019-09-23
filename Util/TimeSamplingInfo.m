187398d669586de98624506734a534fd646c30ef
Hugo <ocehugo@gmail.com>
2019-09-23 16:52:58 +1000

    include testfile for Repeated times in JFE

--- Util/TimeSamplingInfo.m
+++ Util/TimeSamplingInfo.m
@@ -28,24 +28,10 @@ function [info] = TimeSamplingInfo(t, prec),
     %     .jump_backward_indexes - as above, but for negative sampling.
     %
     % Example:
+    %
     % info = TimeSamplingInfo([1,2,3,4]);
     % assert(info.uniform_sampling);
     % assert(all([info.unique_sampling,info.monotonic_sampling,info.progressive_sampling]));
-    %
-    % info = TimeSamplingInfo([4,3,2,1,-1]);
-    % assert(~info.uniform_sampling);
-    % assert(all(info.sampling_steps==[-2,-1]));
-    % assert(all(info.sampling_steps_median==-1));
-    % assert(info.regressive_sampling);
-    % assert(all([info.unique_sampling,info.monotonic_sampling]));
-    %
-    % info = TimeSamplingInfo([1,2,3,5]);
-    % assert(info.non_uniform_sampling_indexes==3);
-    %
-    % info = TimeSamplingInfo([1,2,3,3]);
-    % assert(~info.uniform_sampling);
-    % assert(~all([info.unique_sampling,info.monotonic_sampling]));
-    % assert(info.non_uniform_sampling_indexes==[3]);
     %
     % info = TimeSamplingInfo([1,2,1,3,1,4,1,5,1]);
     % assert(~all([info.unique_sampling,info.progressive_sampling,info.regressive_sampling,info.monotonic_sampling]));
