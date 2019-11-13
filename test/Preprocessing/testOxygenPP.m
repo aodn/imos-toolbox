3e769572df16bb0d6ea74c94e3c188d55b2faa21
Hugo Oliveira <ocehugo@gmail.com>
2019-11-13 18:44:57 +1100

    feat(test): modernize older tests
    This commit rewrites the older
    function tests as class tests for unittest usage.
    
    runAllTests is also now the simple
    wrapper to run all the tests in the test folder.

--- test/Preprocessing/testOxygenPP.m
+++ test/Preprocessing/testOxygenPP.m
@@ -1,81 +1,93 @@
-function testOxygenPP()
-%TESTOXYGENPP exercises the function oxygenPP. This function is a 
-% pre-processing routine that adds oxygen parameters to the dataset.
-%
-% Author:       Peter Jansen <peter.jansen@csiro.au>
-% Contributor:  Guillaume Galibert <guillaume.galibert@utas.edu.au>
-%
+classdef testOxygenPP < matlab.unittest.TestCase
 
-%
-% Copyright (C) 2017, Australian Ocean Data Network (AODN) and Integrated 
-% Marine Observing System (IMOS).
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation version 3 of the License.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-% GNU General Public License for more details.
+    % Test Pre-processing Oxygen function
+    %
+    % refactored version of testOxygenPP.m
+    % by hugo.oliveira@utas.edu.au
+    %
+    % Author:       Peter Jansen <peter.jansen@csiro.au>
+    % Contributor:  Guillaume Galibert <guillaume.galibert@utas.edu.au>
+    %
 
-% You should have received a copy of the GNU General Public License
-% along with this program.
-% If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
-%
-clear sample_data;
+    % Copyright (C) 2019, Australian Ocean Data Network (AODN) and Integrated
+    % Marine Observing System (IMOS).
+    %
+    % This program is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation version 3 of the License.
+    %
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    % GNU General Public License for more details.
+    %
+    % You should have received a copy of the GNU General Public License
+    % along with this program.
+    % If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+    %
 
-testDir = fullfile('test', 'oxygenPP');
+    properties (TestParameter)
+        mode = {'timeSeries'};
+    end
 
-% Test 1: Oxygen Solubility from TEMP and PSAL
-sample_data{1} = SBE19Parse({fullfile(testDir, 'SBE16plus_example1.cnv')}, 'timeSeries');
+    methods (Test)
 
-sample_data{1}.geospatial_lat_min = -46;
-sample_data{1}.geospatial_lat_max = -46;
-sample_data{1}.geospatial_lon_min = 142;
-sample_data{1}.geospatial_lon_max = 142;
+        function testOxygenSolubilityFromTempSalt(~, mode)
+            file = [toolboxRootPath 'data/testfiles/Sea_Bird_Scientific/SBE/16plus/v000/SBE16plus_oxygen1.cnv'];
+            sample_data{1} = SBE19Parse({file}, mode);
+            sample_data{1} = fill_geoinfo(sample_data{1});
+            sample_data = oxygenPP(sample_data, 'qc');
+            sample_data = sample_data{1};
 
-sample_data = oxygenPP(sample_data, 'qc');
+            oxsolSurfSBE = get_data(sample_data, 'OXSOL_SURFACE');
+            dox2SBE = get_data(sample_data, 'DOX2');
+            doxsSBE = get_data(sample_data, 'DOXS');
 
-oxsolSurfSBE = sample_data{1}.variables{getVar(sample_data{1}.variables, 'OXSOL_SURFACE')}.data;
-dox2SBE      = sample_data{1}.variables{getVar(sample_data{1}.variables, 'DOX2')}.data;
-doxsSBE      = sample_data{1}.variables{getVar(sample_data{1}.variables, 'DOXS')}.data;
+            assert(any(abs(oxsolSurfSBE - [262.05291; 271.6910; 247.8694; 250.209]) <= 1e-2), 'Failed: Oxygen Solubility from SBE TEMP and PSAL Check');
 
-assert(any(abs(oxsolSurfSBE - [262.05291; 271.6910;  247.8694;  250.209])   <= 1e-2), 'Failed: Oxygen Solubility from SBE TEMP and PSAL Check');
+            assert(any(abs(dox2SBE - [244.745; 596.267; 244.8609; 244.8362]) <= 1e-2), 'Failed: DOX2 from SBE DOX Check');
 
-% Test 2: DOX2 from DOX
-assert(any(abs(dox2SBE      - [244.745;   596.267;   244.8609;  244.8362])  <= 1e-2), 'Failed: DOX2 from SBE DOX Check');
+            assert(any(abs(doxsSBE / 100 - [0.93395; 2.1946267; 0.9878625; 0.9785269]) <= 1e-4), 'Failed: DOXS from SBE DOX Check');
+        end
 
-% Test 3: DOXS from DOX
-assert(any(abs(doxsSBE/100  - [0.93395;   2.1946267; 0.9878625; 0.9785269]) <= 1e-4), 'Failed: DOXS from SBE DOX Check');
+        function testDOX2fromDOX1_SBE(~, mode)
+            file = [toolboxRootPath 'data/testfiles/Sea_Bird_Scientific/SBE/16plus/v000/SBE16plus_oxygen2.cnv'];
+            sample_data{1} = SBE19Parse({file}, mode);
+            sample_data{1} = fill_geoinfo(sample_data{1});
+            sample_data = oxygenPP(sample_data, 'qc');
+            sample_data = sample_data{1};
 
-% test 4: DOX2 from DOX1
-clear sample_data;
-sample_data{1} = SBE19Parse({fullfile(testDir, 'SBE16plus_example2.cnv')}, 'timeSeries');
+            dox2SBE = get_data(sample_data, 'DOX2');
+            assert(any(abs(dox2SBE - 244.7239) <= 1e-2), 'Failed: DOX2 from SBE DOX1 Check');
+        end
 
-sample_data{1}.geospatial_lat_min = -46;
-sample_data{1}.geospatial_lat_max = -46;
-sample_data{1}.geospatial_lon_min = 142;
-sample_data{1}.geospatial_lon_max = 142;
+        function testDOX2fromDOXS_SBE(~, mode)
+            file = [toolboxRootPath 'data/testfiles/Sea_Bird_Scientific/SBE/16plus/v000/SBE16plus_oxygen3.cnv'];
+            sample_data{1} = SBE19Parse({file}, mode);
+            sample_data{1} = fill_geoinfo(sample_data{1});
+            sample_data = oxygenPP(sample_data, 'qc');
+            sample_data = sample_data{1};
 
-sample_data = oxygenPP(sample_data, 'qc');
+            dox2SBE = get_data(sample_data, 'DOX2');
+            assert(any(abs(dox2SBE - 235.6999) <= 1e-2), 'Failed: DOX2 from SBE DOXS Check');
+        end
 
-dox2SBE = sample_data{1}.variables{getVar(sample_data{1}.variables, 'DOX2')}.data;
+    end
 
-assert(any(abs(dox2SBE - 244.7239) <= 1e-2), 'Failed: DOX2 from SBE DOX1 Check');
-
-% test 5: DOX2 from DOXS
-clear sample_data;
-sample_data{1} = SBE19Parse({fullfile(testDir, 'SBE16plus_example3.cnv')}, 'timeSeries');
-
-sample_data{1}.geospatial_lat_min = -46;
-sample_data{1}.geospatial_lat_max = -46;
-sample_data{1}.geospatial_lon_min = 142;
-sample_data{1}.geospatial_lon_max = 142;
-
-sample_data = oxygenPP(sample_data, 'qc');
-
-dox2SBE = sample_data{1}.variables{getVar(sample_data{1}.variables, 'DOX2')}.data;
-
-assert(any(abs(dox2SBE - 235.6999) <= 1e-2), 'Failed: DOX2 from SBE DOXS Check');
 end
+
+function [sample_data] = fill_geoinfo(sample_data)
+% just fill some info required
+% by oxygenPP
+sample_data.geospatial_lat_min = -46;
+sample_data.geospatial_lat_max = -46;
+sample_data.geospatial_lon_min = 142;
+sample_data.geospatial_lon_max = 142;
+end
+
+function [data] = get_data(sample_data, vname)
+% just bypass
+varid = getVar(sample_data.variables, vname);
+data = sample_data.variables{varid}.data;
+end
+
