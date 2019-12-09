096adc725bede3885e325e731765e51bd8f46fe6
Hugo Oliveira <ocehugo@gmail.com>
2019-12-09 15:44:44 +1100

    feat(Parsers): Staroddi Generic parser & tests
    This include a new Staroddi parser with the new
    Generic Parser machinery.
    
    The machinery requires a set of rules and a parser
    function. See both files for help in usage.
    
    The parser allow processing of:
    
    Starmon Mini v000
    Starmon Mini v001
    Starmon DST Tilt
    Starmon DST CTD
    
    See testStartoddi.m
    
    Given the non-structure nature of the headers,
    the set of rules is a bit big so StaroddiRules
    is heavily commented. This is also the case
    since this is the first example of the generic parser machinery.

---
+++ Parser/GenericParser/InstrumentParsers/StaroddiParser.m
@@ -0,0 +1,185 @@
+function [sample_data, sdata] = StaroddiParser(filename, mode)
+% function [sample_data] = StaroddiParser(filename, mode)
+%
+% Read data from a Star-oddi Instrument series.
+%
+% Inputs:
+%
+% filename - a string or 1x1 cell with the filename string.
+% mode - a string  - IMOS toolbox mode [`timeSeries',`profile`]
+%
+% Outputs:
+%
+% sample_data - the expected IMOS toolbox structure
+% sdata - a structure with all the header/data information
+% sdata.file_encoding - the encoding used
+% sdata.file_machineformat - the machineformat used
+% sdata.header_def_rules - the header definition rules for reading
+% sdata.header_oper_rules - as above, but for conversion
+% sdata.data_def_rules - the data definition rules for reading
+% sdata.data_oper_rules - as above, but for conversion
+% sdata.header_lines - the raw header lines
+% sdata.header_content - the header info after reading
+% sdata.header_info - the header info after processing
+% sdata.raw_data - the data information after reading
+% sdata.proc_data - the data information after processing
+%
+% Example:
+%
+% idata = StaroddiParser({'0-4T3769.DAT'},'timeSeries');
+%
+% author: hugo.oliveira@utas.edu.au
+%
+
+% Copyright (C) 2019, Australian Ocean Data Network (AODN) and Integrated
+% Marine Observing System (IMOS).
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation version 3 of the License.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.
+% If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+%
+
+narginchk(1, 2);
+
+if iscellstr(filename) || isstring(filename)
+    filename = filename{1};
+end
+
+if ~strcmp(mode, 'timeSeries') &&~strcmp(mode, 'Profile')
+    error('mode is not a valid string')
+end
+
+[sdata] = StaroddiRules();
+
+sdata.file_name = filename;
+sdata.mode = mode;
+[sdata.file_encoding, sdata.file_machineformat] = detectEncoding(filename);
+
+fid = fopen(filename, 'r', sdata.file_machineformat, sdata.file_encoding);
+[sdata.header_info, sdata.header_content, sdata.header_lines] = HeaderParser(fid, sdata.header_def_rules, sdata.header_oper_rules);
+[sdata.rawdata, sdata.procdata] = DataParser(fid, sdata);
+data = mapDataNames(sdata.procdata, sdata.name_map_rules, sdata.header_info);
+sdata.data = data;
+fclose(fid);
+
+% map variable names
+hvmap = containers.Map();
+hvmap('Temperature') = 'TEMP';
+hvmap('Pressure') = 'PRES';
+hvmap('Depth') = 'DEPTH';
+hvmap('Salinity') = 'PSAL';
+hvmap('Conductivity') = 'CNDC';
+hvmap('SoundVelocity') = 'SOUND_VEL';
+hvmap('Roll') = 'ROLL';
+hvmap('Pitch') = 'PITCH';
+
+%map dimension names
+hdmap = containers.Map();
+hdmap('Temperature') = 1;
+hdmap('Pressure') = 1;
+hdmap('Depth') = 1;
+hdmap('Salinity') = 1;
+hdmap('Conductivity') = 1;
+hdmap('SoundVelocity') = 1;
+hdmap('Roll') = 1;
+hdmap('Pitch') = 1;
+
+sample_data = struct;
+sample_data.toolbox_input_file = filename;
+sample_data.meta.header = sdata.header_info;
+sample_data.meta.instrument_make = 'Star ODDI';
+sample_data.meta.instrument_model = sdata.header_info.recorder.instrument_name;
+sample_data.meta.instrument_serial_no = sdata.header_info.recorder.serial_number;
+sample_data.meta.instrument_sample_interval = median(diff(data.('TIME') * 24 * 3600));
+sample_data.meta.featureType = mode;
+
+[coordinates, sample_data.dimensions, sample_data.variables] = loadTimeSeriesSampleTemplate(data.('TIME'));
+converted_variables = convertVariables(data, hvmap, hdmap, coordinates);
+
+sample_data.variables = [sample_data.variables, converted_variables];
+
+is_degF_mini = isfield(sdata.header_info, 'channel_index_1') && isfield(sdata.header_info,'channel_info_1') && strcmp(sdata.header_info.channel_info_1.channel_units, '°F');
+is_degF_dst = isfield(sdata.header_info, 'axis_index_0') && isfield(sdata.header_info,'axis_info_0') && strcmp(sdata.header_info.axis_info_0.axis_units, '°F');
+is_temp_fahrenheit = is_degF_mini || is_degF_dst;
+
+if is_temp_fahrenheit
+    for k = 1:length(sample_data.variables)
+        vname = sample_data.variables{k}.name;
+        is_temp = strcmp(vname, 'TEMP') || contains(vname, 'TEMP_');
+
+        if is_temp
+            sample_data.variables{k}.data = toCelsius(sample_data.variables{k}.data,'fahrenheit');
+            sample_data.variables{k}.comment = 'Originaly expressed in Fahrenheit.';
+        end
+
+    end
+
+end
+
+is_temp_corrected = isfield(sdata.header_info, 'no_temperature_correction') && ~sdata.header_info.no_temperature_correction;
+is_pres_corrected = isfield(sdata.header_info, 'pressure_offset_correction') && sdata.header_info.pressure_offset_correction;
+is_salt_corrected = (is_temp_corrected || is_pres_corrected) && getVar(sample_data.variables, 'PSAL');
+
+if is_temp_corrected
+    k = getVar(sample_data.variables, 'TEMP_2');
+    if k>0
+        cmt = sample_data.variables{k}.comment;
+        sample_data.variables{k}.comment = [cmt 'Normal temperature correction applied.'];
+    else
+        error("Filename %s do not contain corrected temperature field");
+    end
+end
+
+if is_pres_corrected
+    k = getVar(sample_data.variables, 'PRES');
+    if k>0
+        pres_rel = sample_data.variables{k};
+        pres_rel.name = 'PRES_REL';
+        pres_rel.comment = ['A zero offset of value ' sdata.header_info.pressure_offset_correction 'mbar was adjusted.'];
+        pres_rel.applied_offset = sdata.header_info.pressure_offset_correction / 100;
+        sample_data.variables{end + 1} = pres_rel;
+    else
+        error("Filename %s do not contain corrected pressure field");
+    end
+
+
+    k = getVar(sample_data.variables, 'DEPTH');
+    if k>0
+        depth_rel = sample_data.variables{k};
+        depth_rel.name = 'DEPTH_2';
+        depth_rel.comment = ['A zero offset of value ' sdata.header_info.pressure_offset_correction 'mbar was adjusted to pressure.'];
+        sample_data.variables{end + 1} = depth_rel;
+    end
+end
+
+if is_salt_corrected
+    k = getVar(sample_data.variables, 'PSAL');
+    if k>0
+        psal_rel = sample_data.variables{k};
+        psal_rel.name = 'PSAL_2';
+
+        if is_temp_corrected
+            psal_rel.comment = 'Normal temperature correction applied.';
+        end
+
+        if is_pres_corrected
+            psal_rel.comment = [psal_rel.comment 'A zero offset of value ' sdata.header_info.pressure_offset_correction 'mbar was adjusted to pressure.'];
+        end
+
+        sample_data.variables{end + 1} = psal_rel;
+    else
+        error("Filename %s do not contain corrected salinity field");
+    end
+
+end
+
+end
---
+++ test/testStaroddi.m
@@ -0,0 +1,249 @@
+classdef testStaroddi < matlab.unittest.TestCase
+
+    properties (TestParameter)
+        mode = struct('timeSeries', 'timeSeries'); %, 'profile', 'profile');
+        starmon_mini_v000_param = get_testfiles('Star_oddi', 'mini', 'v000');
+        starmon_mini_v000_folder = {get_folder('Star_oddi', 'mini', 'v000')};
+
+        starmon_mini_v001_param = get_testfiles('Star_oddi', 'mini', 'v001');
+        starmon_mini_v001_folder = {get_folder('Star_oddi', 'mini', 'v001')};
+
+        starmon_dsttilt_v000_param = get_testfiles('Star_oddi', 'dst_tilt', 'v000');
+        starmon_dsttilt_v000_folder = {get_folder('Star_oddi', 'dst_tilt', 'v000')};
+
+        starmon_dstctd_v000_param = get_testfiles('Star_oddi', 'dst_ctd', 'v000');
+        starmon_dstctd_v000_folder = {get_folder('Star_oddi', 'dst_ctd', 'v000')};
+
+    end
+
+    methods (Test)
+        %%TODO REMOVE after old parsers are deprecated.
+        % function testCreateMatfilesMiniv000(~, starmon_mini_v000_param, starmon_mini_v000_folder, mode)
+        %     save_mat(StarmonMiniParse({starmon_mini_v000_param}, mode), starmon_mini_v000_param, fullfile(starmon_mini_v000_folder, 'mat'))
+        % end
+
+        %%TODO REMOVE after old parsers are deprecated.
+        % function testCreateMatfilesDSTTiltv000(~, starmon_dsttilt_v000_param, starmon_dsttilt_v000_folder, mode)
+        %     save_mat(StarmonDSTParse({starmon_dsttilt_v000_param}, mode), starmon_dsttilt_v000_param, fullfile(starmon_dsttilt_v000_folder, 'mat'));
+        % end
+
+        %%TODO REMOVE after old parsers are deprecated.
+        % function testCreateMatfilesDSTCTDv000(~, starmon_dstctd_v000_param, starmon_dstctd_v000_folder, mode)
+        %     save_mat(StarmonDSTParse({starmon_dstctd_v000_param}, mode), starmon_dstctd_v000_param, fullfile(starmon_dstctd_v000_folder, 'mat'));
+        % end
+
+        function testCompareOldNewParser_mini_v000(~, starmon_mini_v000_param, starmon_mini_v000_folder, mode)
+            old_data = load([fullfile(starmon_mini_v000_folder, 'mat', basename(starmon_mini_v000_param)) '.mat']);
+            new_data = StaroddiParser(starmon_mini_v000_param, mode);
+
+            assert(strcmp(new_data.toolbox_input_file, old_data.data.toolbox_input_file));
+            [old_meta, old_dimensions, old_variables] = loadfields(old_data.data);
+            [new_meta, new_dimensions, new_variables] = loadfields(new_data);
+
+            [isdiff_meta, why] = treeDiff(old_meta, new_meta);
+
+            if isdiff_meta
+                assert(isdiff_meta, why);
+            end
+
+            [isdiff_dim, why] = treeDiff(old_dimensions, new_dimensions);
+
+            if isdiff_dim
+                assert(isdiff_dim, why);
+            end
+
+            [isdiff_var, why] = treeDiff(old_variables, new_variables);
+
+            if isdiff_var
+                assert(isdiff_var, why);
+            end
+
+        end
+
+        function testCompareOldNewParser_dsttilt_v000(~, starmon_dsttilt_v000_param, starmon_dsttilt_v000_folder, mode)
+            old_data = load([fullfile(starmon_dsttilt_v000_folder, 'mat', basename(starmon_dsttilt_v000_param)) '.mat']);
+            new_data = StaroddiParser(starmon_dsttilt_v000_param, mode);
+
+            assert(strcmp(new_data.toolbox_input_file, old_data.data.toolbox_input_file));
+            [old_meta, old_dimensions, old_variables] = loadfields(old_data.data);
+            [new_meta, new_dimensions, new_variables] = loadfields(new_data);
+
+            [isdiff_meta, why] = treeDiff(old_meta, new_meta);
+
+            if isdiff_meta
+                assert(isdiff_meta, why);
+            end
+
+            [isdiff_dim, why] = treeDiff(old_dimensions, new_dimensions);
+
+            if isdiff_dim
+                assert(isdiff_dim, why);
+            end
+
+            [isdiff_var, why] = treeDiff(old_variables, new_variables);
+
+            if isdiff_var
+                assert(isdiff_var, why);
+            end
+
+        end
+
+        function testCompareOldNewParser_dstctd_v000(~, starmon_dstctd_v000_param, starmon_dstctd_v000_folder, mode)
+            old_data = load([starmon_dstctd_v000_folder '/mat/' basename(starmon_dstctd_v000_param) '.mat']);
+            new_data = StaroddiParser(starmon_dstctd_v000_param, mode);
+
+            assert(strcmp(new_data.toolbox_input_file, old_data.data.toolbox_input_file));
+            [old_meta, old_dimensions, old_variables] = loadfields(old_data.data);
+            [new_meta, new_dimensions, new_variables] = loadfields(new_data);
+
+            [isdiff_meta, why] = treeDiff(old_meta, new_meta);
+
+            if isdiff_meta
+                assert(isdiff_meta, why);
+            end
+
+            [isdiff_dim, why] = treeDiff(old_dimensions, new_dimensions);
+
+            if isdiff_dim
+                assert(isdiff_dim, why);
+            end
+
+            [isdiff_var, why] = treeDiff(old_variables, new_variables);
+
+            if isdiff_var
+                assert(isdiff_var, why);
+            end
+
+        end
+
+        function testStaroddiParser_temp_degF(~, mode)
+            root_folder = toolboxRootPath();
+            file = fullfile(root_folder, 'data', 'testfiles', 'Star_oddi', 'mini', 'v000', '6T3863_degF.DAT');
+            new_data = StaroddiParser(file, mode);
+            varid = getVar(new_data.variables, 'TEMP');
+            var = new_data.variables{varid};
+            assert(contains(var.comment, 'expressed in Fahrenheit'));
+            assert(min(var.data) >= 25.03);
+            assert(max(var.data) <= 25.56);
+        end
+
+        function testStaroddiParser_temp_correction(~, mode)
+            root_folder = toolboxRootPath();
+            file = fullfile(root_folder, 'data', 'testfiles', 'Star_oddi', 'mini', 'v000', '6T3863_t_reconvert.DAT');
+            [new_data, sdata] = StaroddiParser(file, mode);
+
+            % temp correction
+            is_temp_corrected = isfield(sdata.header_info, 'no_temperature_correction') &&~sdata.header_info.no_temperature_correction;
+            assert(is_temp_corrected)
+
+            varid = getVar(new_data.variables, 'TEMP_2');
+            var = new_data.variables{varid};
+            assert(contains(var.comment, 'correction applied'));
+        end
+
+        function testStaroddiParser_pres_correction(~, mode)
+            root_folder = toolboxRootPath();
+            file = fullfile(root_folder, 'data', 'testfiles', 'Star_oddi', 'dst_ctd', 'v000', '42S8171_correction.DAT');
+            [new_data, sdata] = StaroddiParser(file, mode);
+
+            % pres correction
+            is_pres_corrected = isfield(sdata.header_info, 'pressure_offset_correction') && sdata.header_info.pressure_offset_correction;
+            assert(is_pres_corrected)
+
+            varid = getVar(new_data.variables, 'PRES_REL');
+            var = new_data.variables{varid};
+            assert(contains(var.comment, 'adjusted'));
+        end
+
+        function testStaroddiParser_psal_correction(~, mode)
+            root_folder = toolboxRootPath();
+            file = fullfile(root_folder, 'data', 'testfiles', 'Star_oddi', 'dst_ctd', 'v000', '42S8171_correction.DAT');
+            [new_data, sdata] = StaroddiParser(file, mode);
+
+            % pres correction
+            is_pres_corrected = isfield(sdata.header_info, 'pressure_offset_correction') && sdata.header_info.pressure_offset_correction;
+            assert(is_pres_corrected)
+
+            varid = getVar(new_data.variables, 'PRES_REL');
+            var = new_data.variables{varid};
+            assert(contains(var.comment, 'mbar was adjusted'));
+
+            varid = getVar(new_data.variables, 'PSAL_2');
+            var = new_data.variables{varid};
+            assert(contains(var.comment, 'mbar was adjusted to pressure'));
+        end
+
+        function testStaroddiParser_mini_v000(~, starmon_mini_v000_param, mode)
+            new_data = StaroddiParser(starmon_mini_v000_param, mode);
+            assert(strcmp(new_data.meta.instrument_make, 'Star ODDI'));
+            assert(strcmp(new_data.meta.instrument_model, 'Starmon Mini'));
+            assert(length(new_data.dimensions) == 1);
+            assert(length(new_data.variables) > 4);
+        end
+
+        function testStaroddiParser_mini_v001(~, starmon_mini_v001_param, mode)
+            new_data = StaroddiParser(starmon_mini_v001_param, mode);
+            assert(strcmp(new_data.meta.instrument_make, 'Star ODDI'));
+            assert(strcmp(new_data.meta.instrument_model, 'Starmon mini')); % yes - mini lowercase now...
+            assert(length(new_data.dimensions) == 1);
+            assert(length(new_data.variables) > 4);
+        end
+
+        function testStaroddiParser_dsttilt_v000(~, starmon_dsttilt_v000_param, mode)
+            new_data = StaroddiParser(starmon_dsttilt_v000_param, mode);
+            assert(strcmp(new_data.meta.instrument_make, 'Star ODDI'));
+            assert(contains(new_data.meta.instrument_model, 'DST Tilt'));
+            assert(length(new_data.dimensions) == 1);
+            assert(length(new_data.variables) > 4);
+        end
+
+        function testStaroddiParser_dstctd_v000(~, starmon_dstctd_v000_param, mode)
+            new_data = StaroddiParser(starmon_dstctd_v000_param, mode);
+            assert(strcmp(new_data.meta.instrument_make, 'Star ODDI'));
+            assert(contains(new_data.meta.instrument_model, 'DST CTD'));
+            assert(length(new_data.dimensions) == 1);
+            assert(length(new_data.variables) > 4);
+        end
+
+    end
+
+end
+
+function [m, d, v] = loadfields(idata)
+%
+% grab metadata, dimensons and variables
+% A one-liner to save 4 lines.
+%
+m = rmfield(idata.meta, 'header');
+m = orderfields(m); % required for tree matching.
+d = idata.dimensions;
+v = idata.variables;
+end
+
+function [param, files, folder] = get_testfiles(maker, model, version)
+%
+% get param,files, and folder testfiles information for a certain
+% instrument maker/model and version.
+%
+root_folder = toolboxRootPath();
+folder = fullfile(root_folder, 'data', 'testfiles', maker, model, version);
+files = FilesInFolder(folder);
+param = files2namestruct(files);
+end
+
+function [folder] = get_folder(maker, model, version)
+%
+% wrapper to obtain only folder
+%
+[~, ~, folder] = get_testfiles(maker, model, version);
+end
+
+function save_mat(data, tparam, outfolder)
+%
+% save a mat file to outfolder/tparam.mat
+%
+fs = char(tparam);
+file = [basename(fs) '.mat'];
+omat = fullfile(outfolder, file);
+save(omat, 'data');
+end
