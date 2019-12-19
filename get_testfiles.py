b58c2ce7943ef6665f5ccce3b44226726e69429b
Hugo Oliveira <ocehugo@gmail.com>
2019-12-19 12:20:41 +1100

    feat(refact): remove complexity from __main__

--- get_testfiles.py
+++ get_testfiles.py
@@ -3,7 +3,7 @@
 get_testfiles
 
 Usage:
-  get_testfiles [--outdir=<outdir> (-v | --verbose)]
+  get_testfiles [--outdir=<outdir>]
   get_testfiles (-h | --help)
 
 Options:
@@ -15,7 +15,6 @@ __package__ = 'imos-toolbox'
 
 import hashlib
 import os
-import sys
 
 import boto3
 from botocore.exceptions import ClientError
@@ -24,7 +23,7 @@ from docopt import docopt
 
 DEFAULT_BUCKET = 'imos-toolbox'
 DEFAULT_OUTDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
-NTRY = 10
+DOWNLOAD_ATTEMPTS = 10
 
 
 def file_md5(fname: str) -> str:
@@ -44,7 +43,7 @@ def check_md5(
     return file_md5(file) == md5
 
 
-def get_md5(client, bucket: str, keyname: str) -> str:
+def get_md5(client: boto3.client, bucket: str, keyname: str) -> str:
     """Get the md5 of a key in s3"""
     metadata = client.head_object(Bucket=bucket, Key=keyname)
     etag = metadata['ETag'][1:-1]
@@ -55,11 +54,90 @@ def get_md5(client, bucket: str, keyname: str) -> str:
     return md5
 
 
+def gen_param(bucket: boto3.resource) -> (str, str, str):
+    """
+    Generate the main 3 parameters to save a s3 file locally:
+        kname - s3 key name
+        dest_folder - destination folder name
+        dest_file - destination file name
+    """
+    for s3obj in bucket.objects.all():
+        kname = s3obj.key
+        path, filename = os.path.split(kname)
+        dest_file = os.path.join(outdir, kname)
+        dest_folder = os.path.join(outdir, path)
+        yield kname, dest_folder, dest_file
+
+
+def need_md5_check(client: boto3.client, bucket_name: str,
+                   kname: str) -> (str, bool):
+    """
+    Return a boolean if the file need/can be md5sum
+    check, as well as the actual md5sum of the file.
+    """
+    try:
+        md5sum = get_md5(client, DEFAULT_BUCKET, kname)
+        do_md5_check = True
+    except (ClientError, KeyError):
+        md5sum = None
+        do_md5_check = False
+
+    return do_md5_check, md5sum
+
+
+def should_skip_file(dest_file: str, do_md5_check: bool, md5sum: str) -> bool:
+    """
+    Determine if the file should be downloaded or not, based on
+    file existance, capacity for md5sum check, and actual md5sum
+    of local and remote file key.
+    """
+    if os.path.exists(dest_file):
+        if do_md5_check:
+            if check_md5(dest_file, md5sum):
+                print(f"m5dsum match - Skipping {dest_file}")
+                return True
+            else:
+                print(f"md5sum diff - Overwriting {dest_file}")
+                return False
+    return False
+
+
+def download_file(client: boto3.client,
+                  bucket_name: str,
+                  kname: str,
+                  dest_file: str,
+                  do_md5_check: bool = False,
+                  md5sum: str = '',
+                  attempts: int = DOWNLOAD_ATTEMPTS) -> None:
+    ntry = 0
+    while ntry < attempts:
+        ntry += 1
+        try:
+            print(f'Downloading {dest_file} | {md5sum}')
+            client.download_file(DEFAULT_BUCKET, kname, dest_file)
+        except ClientError:
+            continue
+
+        if not do_md5_check:
+            break
+        else:
+            if check_md5(dest_file, md5sum):
+                print(f'{dest_file} pass md5sum -> DONE!')
+                break
+            else:
+                print(f'{dest_file} fail md5sum -> Trying again...')
+
+    if ntry == attempts:
+        print(f"Try attempt exceeded for s3 key: {kname}")
+        return False
+    return True
+
+
 if __name__ == '__main__':
 
     args = docopt(__doc__, version='get_testfiles 0.1')
     outdir = args['--outdir']
-    verbose = args['--verbose'] or args['-v']
+
     if not outdir:
         outdir = DEFAULT_OUTDIR
 
@@ -69,57 +147,22 @@ if __name__ == '__main__':
     s3.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
     bucket = s3.Bucket(DEFAULT_BUCKET)
 
-    if verbose:
-        print(f"Getting test files in {DEFAULT_BUCKET} bucket...")
+    print(f"Getting test files in {DEFAULT_BUCKET} bucket...")
 
-    for s3obj in bucket.objects.all():
-        kname = s3obj.key
-        path, filename = os.path.split(kname)
-        dest_file = os.path.join(outdir, kname)
-        dest_folder = os.path.join(outdir, path)
-
-        try:
-            md5sum = get_md5(client, DEFAULT_BUCKET, kname)
-            do_md5_check = True
-        except (ClientError, KeyError):
-            md5sum = None
-            do_md5_check = False
-
+    for kname, dest_folder, dest_file in gen_param(bucket):
+        do_md5_check, md5sum = need_md5_check(client, DEFAULT_BUCKET, kname)
+        
         if not os.path.exists(dest_folder):
-            if verbose:
-                print(f"Creating folder {dest_folder}")
+            print(f"Creating folder {dest_folder}")
             os.makedirs(dest_folder)
 
-        if os.path.exists(dest_file):
-            if do_md5_check:
-                if check_md5(dest_file, md5sum):
-                    if verbose:
-                        print(f"m5dsum match - Skipping {dest_file}")
-                    continue
-                else:
-                    if verbose:
-                        print(f"md5sum diff - Overwriting {dest_file}")
-
-        ntry = 0
-        while ntry < NTRY:
-            ntry += 1
-            try:
-                print(f'Downloading {dest_file} | {md5sum}')
-                client.download_file(DEFAULT_BUCKET, kname, dest_file)
-            except ClientError:
-                __import__('pdb').set_trace()
-                continue
-
-            if not do_md5_check:
-                break
-            else:
-                if check_md5(dest_file, md5sum):
-                    print(f'{dest_file} pass md5sum -> DONE!')
-                    break
-                else:
-                    print(f'{dest_file} fail md5sum -> Trying again...')
-
-        if ntry == NTRY:
-            print('Aborted - try attempt exceeded')
-            sys.exit(-1)
+        skip = should_skip_file(dest_file, do_md5_check, md5sum)
+        if not skip:
+            download_file(client,
+                          DEFAULT_BUCKET,
+                          kname,
+                          dest_file,
+                          do_md5_check=do_md5_check,
+                          md5sum=md5sum,
+                          attempts=DOWNLOAD_ATTEMPTS)
     print(f'SUCCESS syncing {DEFAULT_BUCKET}')
