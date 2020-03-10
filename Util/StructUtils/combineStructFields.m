43b0ec8f4625fe850b9f84cd3a5a3e40d5d9474a
Hugo Oliveira <ocehugo@gmail.com>
2020-03-10 13:04:57 +1100

    feat(refact): add suport to multi dim structs

--- Util/StructUtils/combineStructFields.m
+++ Util/StructUtils/combineStructFields.m
@@ -5,7 +5,7 @@ function [combined] = combineStructFields(varargin)
     % the different fieldnames in the argument structures.
     %
     % Inputs:
-
+    %
     % any number of structures
     %
     % Outputs:
@@ -13,12 +13,29 @@ function [combined] = combineStructFields(varargin)
     % combined - a structure with all fieldnames of the arguments combined
     %
     % Example:
+    % % basic case
+    % a = struct(); b=a;
     % a.x = 1;
     % b.x = 2;
     % b.y = 3;
     % combined = combineStructFields(a,b);
-    % assert(combined.x == 2);
-    % assert(combined.y == 3);
+    % assert(combined.x==2);
+    % assert(combined.y==3);
+    %
+    % % multi structure case
+    % a = struct(); b=a; c=a;
+    % a.x = 1;
+    % b.x = 2;
+    % b(2).x = 3;
+    % b(3).x = 4;
+    % c.x = 3;
+    % c(2).x = 4;
+    % c(3).x = 5;
+    % combined = combineStructFields(a,b,c);
+    % assert(isequal(size(combined),[1,3]))
+    % assert(combined(1).x==3)
+    % assert(combined(2).x==4)
+    % assert(combined(3).x==5)
     %
     % author: hugo.oliveira@utas.edu.au
 
@@ -42,15 +59,15 @@ function [combined] = combineStructFields(varargin)
 
     combined = struct();
 
-    for k = 1:nargin
-        s = varargin{k};
-        snames = fieldnames(s);
-
-        for kk = 1:length(snames)
-            name = snames{kk};
-            combined.(name) = s.(name);
+    for n = 1:nargin
+        arg = varargin{n};
+        snames = fieldnames(arg);
+        for s = 1:numel(arg)
+            for k = 1:length(snames)
+                name = snames{k};
+                combined(s).(name) = arg(s).(name);
+            end
         end
-
     end
 
 end
