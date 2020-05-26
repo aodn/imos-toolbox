6ade4aabb58a3d2f823c470f1467f99de75c26d8
Hugo Oliveira <ocehugo@gmail.com>
2020-05-26 14:11:09 +1000

    feat(QC): update tilt handling and values
    This updates imosTiltVelocitySetQC to be
    usable for aquadopp current meters.
    
    1. The table & comments are updated
    2. The code is changed to allow perfect matches to table entries,
    then partial matches, then inverted matches. This is required for
    more flexibility and to avoid breaking previous workflows.
    3. A dialog box pops-up to confirm values/allow editing in place instead
    of editing defaults.

--- AutomaticQC/imosTiltVelocitySetQC.m
+++ AutomaticQC/imosTiltVelocitySetQC.m
@@ -1,18 +1,20 @@
 function [sample_data, varChecked, paramsLog] = imosTiltVelocitySetQC( sample_data, auto )
-%IMOSTILTVELOCITYSETQC Quality control procedure for ADCP instrument data against their tilt.
+%IMOSTILTVELOCITYSETQC Quality control procedure for instrument data against their tilt.
 %
-% Quality control ADCP velocity data, assessing the tilt of the instrument.
+% Quality control velocity data, assessing the tilt of the instrument in both
+% ADCP and current meter data.
 %
 % The tilt of the instrument plays into a number of factors:
-%          -We need to know the tilt in order to convert from along the beam to 
+%          -We need to know the tilt in order to convert from along the beam to
 %           horizontal and vertical components
-%          -We need to know the tilt in order to map the bins from different 
+%          -We need to know the tilt in order to map the bins from different
 %           beams to the same place in the water column
 %          -The compass errors increase with increasing tilts.
 %
 % Finally, most tilt sensors output is scaled and reaches a maximum at
-% some specific angle so that any measured angle equals or greater than this 
+% some specific angle so that any measured angle equals or greater than this
 % maximum means that the measured velocity data is not reliable.
+%
 %
 % Inputs:
 %   sample_data - struct containing the entire data set and dimension data.
@@ -24,73 +26,74 @@ function [sample_data, varChecked, paramsLog] = imosTiltVelocitySetQC( sample_da
 %   varChecked  - cell array of variables' name which have been checked
 %   paramsLog   - string containing details about params' procedure to include in QC log
 %
+%
 % More RDI details below from Darryl.Symonds@Teledyne.com on Thu 18/12/2014 :
 %
-% The Sentinel V, can provide tilts basically in full circle around the instrument. 
+% The Sentinel V, can provide tilts basically in full circle around the instrument.
 % The WorkHorse tilt sensor output is scaled and reaches a maximum at either :
-%   ~22 degrees for Sentinel/Monitor, 
+%   ~22 degrees for Sentinel/Monitor,
 %   ~50 degrees for the Long Ranger and QuarterMaster.
 %
 % Coordinate Transformation
 % -------------------------
-% Converting from along the beams (radial) velocities to horizontal and vertical 
-% components is done using the beam angle of the transducer +/- the tilt of the 
-% instrument.  Very basic trigonometry is applied here.   If the instrument is 
-% tilted over by more than the tilt sensors can measure then (i.e. >22degrees 
-% in a WH) there is no way to resolve the math and so you no longer are sure 
-% that the vertical component and horizontal component have been calculated 
-% properly.  However, if you know something about the area that you are operating 
-% in, and if the vertical component has a mean of zero with some shear up and 
-% down the profile then you can look at your data and see if the vertical 
-% velocities being reported are doing this.  If they are then you can “feel” 
-% good about the data you have.  If however the vertical component appears to 
-% be biased off of zero then you know that the tilts were exceeded by much more 
+% Converting from along the beams (radial) velocities to horizontal and vertical
+% components is done using the beam angle of the transducer +/- the tilt of the
+% instrument.  Very basic trigonometry is applied here.   If the instrument is
+% tilted over by more than the tilt sensors can measure then (i.e. >22degrees
+% in a WH) there is no way to resolve the math and so you no longer are sure
+% that the vertical component and horizontal component have been calculated
+% properly.  However, if you know something about the area that you are operating
+% in, and if the vertical component has a mean of zero with some shear up and
+% down the profile then you can look at your data and see if the vertical
+% velocities being reported are doing this.  If they are then you can “feel”
+% good about the data you have.  If however the vertical component appears to
+% be biased off of zero then you know that the tilts were exceeded by much more
 % than the maximum tilt reported and now you cannot trust the data.
-%  
-% Note this is all subjective and if you are trying to measure to the accuracy 
-% of our specification then you pretty much cannot trust velocity data once 
-% you reach the maximum tilt of the instrument. 
-%  
+%
+% Note this is all subjective and if you are trying to measure to the accuracy
+% of our specification then you pretty much cannot trust velocity data once
+% you reach the maximum tilt of the instrument.
+%
 % Bin Mapping
 % -----------
-% Our bin mapping algorithm uses the tilt data to make sure that the correct 
-% bins along each the beams are used when creating the horizontal velocities. 
-% If you have small tilts, say <5 degrees, then the there is very little “bin 
-% mapping” required until you get very far away from the ADCP.    
-%  
-% To help illustrate this please picture the ADCP so that it tilts only on one 
-% axis.  The tilt now is so great that one beam is horizontal.  It has no 
-% vertical profile at all.  None of its bins will map to the position of the 
+% Our bin mapping algorithm uses the tilt data to make sure that the correct
+% bins along each the beams are used when creating the horizontal velocities.
+% If you have small tilts, say <5 degrees, then the there is very little “bin
+% mapping” required until you get very far away from the ADCP.
+%
+% To help illustrate this please picture the ADCP so that it tilts only on one
+% axis.  The tilt now is so great that one beam is horizontal.  It has no
+% vertical profile at all.  None of its bins will map to the position of the
 % other 3 beams that are pointing somewhat vertically in the water column.
-%  
-% Now as you have less tilt what you will see is that near bins map out in the 
-%same vertical position for all 4 beams.  However, as you move further away 
-% from the instrument the tilt magnifies the difference in vertical position 
-% in the water column and causes one bin a particular beam to be lower than 
-% another bin in a different / opposite beam.  
-%  
-% If the tilt sensor has reached its maximum then this bin mapping cannot be 
-% accurately applied.  The result is that bins from different vertical depths 
-% in the water may be used in the coordinate transformed data.  The result is 
-% that the velocities are smeared over the depth of the instrument.   
-%  
-% Again, if you are trying to measure to the accuracy of our specification 
-% then you pretty much cannot trust velocity data once you reach the maximum 
+%
+% Now as you have less tilt what you will see is that near bins map out in the
+%same vertical position for all 4 beams.  However, as you move further away
+% from the instrument the tilt magnifies the difference in vertical position
+% in the water column and causes one bin a particular beam to be lower than
+% another bin in a different / opposite beam.
+%
+% If the tilt sensor has reached its maximum then this bin mapping cannot be
+% accurately applied.  The result is that bins from different vertical depths
+% in the water may be used in the coordinate transformed data.  The result is
+% that the velocities are smeared over the depth of the instrument.
+%
+% Again, if you are trying to measure to the accuracy of our specification
+% then you pretty much cannot trust velocity data once you reach the maximum
 % tilt of the instrument.
-%  
+%
 % Compass
 % -------
-% Our standard specification for our compass assumes the system will be with 
-% tilts of around +/-15 degrees. The errors in the compass will increase beyond 
-% this and can be in 10’s of degrees rather than the +/-2 degrees you can 
-% achieve with the field calibration.     
-%  
-% The reason is that the flux gate compass is measuring the earth’s horizontal 
-% magnetic field strength.  As you tilt the instrument over the sensors on the 
-% compass are now pointing at a different angle relative the earth’s horizontal 
-% magnetic field and less energy is actually measured. This is the same 
-% phenomenon as what happens as you near the magnetic poles. The difference 
-% is that the earth’s magnetic field is what is changing and at the poles the 
+% Our standard specification for our compass assumes the system will be with
+% tilts of around +/-15 degrees. The errors in the compass will increase beyond
+% this and can be in 10’s of degrees rather than the +/-2 degrees you can
+% achieve with the field calibration.
+%
+% The reason is that the flux gate compass is measuring the earth’s horizontal
+% magnetic field strength.  As you tilt the instrument over the sensors on the
+% compass are now pointing at a different angle relative the earth’s horizontal
+% magnetic field and less energy is actually measured. This is the same
+% phenomenon as what happens as you near the magnetic poles. The difference
+% is that the earth’s magnetic field is what is changing and at the poles the
 % magnetic field becomes vertical.
 %
 %
@@ -98,8 +101,8 @@ function [sample_data, varChecked, paramsLog] = imosTiltVelocitySetQC( sample_da
 %
 % Page 19, chapter 1.1.6.3 Pitch, Roll and Heading
 % ------------------------------------------------
-% Tilt readings between 20° and 30° affect the data accuracy in a way that is 
-% likely to make the data fail to meet the specifications. Data acquired during 
+% Tilt readings between 20° and 30° affect the data accuracy in a way that is
+% likely to make the data fail to meet the specifications. Data acquired during
 % tilts exceeding 30° are in general not reliable and should be discarded.
 %
 %
@@ -107,7 +110,7 @@ function [sample_data, varChecked, paramsLog] = imosTiltVelocitySetQC( sample_da
 %
 
 %
-% Copyright (C) 2017, Australian Ocean Data Network (AODN) and Integrated 
+% Copyright (C) 2017, Australian Ocean Data Network (AODN) and Integrated
 % Marine Observing System (IMOS).
 %
 % This program is free software: you can redistribute it and/or modify
@@ -143,7 +146,6 @@ idRoll = 0;
 lenVar = length(sample_data.variables);
 for i=1:lenVar
     paramName = sample_data.variables{i}.name;
-    
     if strncmpi(paramName, 'UCUR', 4),  idUcur    = i; end
     if strncmpi(paramName, 'VCUR', 4),  idVcur    = i; end
     if strcmpi(paramName, 'WCUR'),      idWcur    = i; end
@@ -169,15 +171,28 @@ if isfield(sample_data, 'meta')
         instrument = [sample_data.meta.instrument_make ' ' sample_data.meta.instrument_model];
     end
 end
-        
-[firstTiltThreshold, secondTiltThreshold, firstFlagThreshold, secondFlagThreshold] = getTiltThresholds(instrument);
 
-if isempty(firstTiltThreshold)
+[matchedName, firstTiltThreshold, secondTiltThreshold, firstFlagThreshold, secondFlagThreshold] = getTiltThresholds(instrument);
+
+if isempty(firstTiltThreshold) && auto
     % couldn't find this instrument so quit the test
     disp(['Warning: imosTiltVelositySetQC could not be performed on ' sample_data.toolbox_input_file ...
         ' instrument = "' instrument '" => Fill imosTiltVelositySetQC.txt with relevant make/model information if you wish to run this test on this dataset.']);
     return;
 end
+
+if ~auto
+  %fire-up a dialog box to confirm values/allow editing.
+  isdeg = @(x)(x>=0 & x<=360);
+  isvalidQC = @(x)(x>=0 & x<=4);
+  names = {'firstTiltThreshold [deg]', 'firstFlagThreshold [imosQCFlag]', 'secondTiltThreshold [deg]', 'secondFlagThreshold [imosQCFlag]'};
+  values = {firstTiltThreshold, firstFlagThreshold, secondTiltThreshold, secondFlagThreshold};
+  funcs = {isdeg, isvalidQC, isdeg, isvalidQC};
+  results = uiNumericalBox(names,values,funcs,'title','imosTiltVelositySetQC - Threshold Limits','panelTitle',matchedName);
+  [firstTiltThreshold,firstFlagThreshold,secondTiltThreshold,secondFlagThreshold] = results{:};
+end
+
+
 
 paramsLog = ['firstTiltThreshold=' num2str(firstTiltThreshold) ', secondTiltThreshold=' num2str(secondTiltThreshold)];
 
@@ -197,10 +212,19 @@ flags = ones(sizeCur, 'int8')*secondFlagThreshold;
 
 % tilt test
 iPass = tilt < secondTiltThreshold;
-flags(iPass,:) = firstFlagThreshold;
+if ~isvector(flags)
+  flags(iPass) = firstFlagThreshold;
+else
+  flags(iPass,:) = firstFlagThreshold;
+end
 
 iPass = tilt < firstTiltThreshold;
-flags(iPass,:) = goodFlag;
+if ~isvector(flags)
+  flags(iPass) = goodFlag;
+else
+  flags(iPass,:) = goodFlag;
+end
+
 
 if idWcur
     sample_data.variables{idWcur}.flags = flags;
@@ -223,13 +247,15 @@ if idCdir
     sample_data.variables{idCdir}.flags = flags;
     varChecked = [varChecked, {sample_data.variables{idCdir}.name}];
 end
+
 end
 
-function [firstTiltThreshold, secondTiltThreshold, firstTiltflag, secondTiltflag] = getTiltThresholds(instrument)
-%GETTILTTHRESHOLDS Returns the 2-level ADCP tilt thresholds  and their
+function [matchedName,firstTiltThreshold, secondTiltThreshold, firstTiltflag, secondTiltflag] = getTiltThresholds(instrument)
+%GETTILTTHRESHOLDS Returns the 2-level tilt thresholds and their
 % associated flags according to the global attribute instrument provided.
 %
 
+matchedName = [];
 firstTiltThreshold  = [];
 secondTiltThreshold = [];
 firstTiltflag  = [];
@@ -241,11 +267,9 @@ if isempty(path), path = pwd; end
 path = fullfile(path, 'AutomaticQC');
 
 fid = -1;
-params = [];
 try
   fid = fopen([path filesep 'imosTiltVelocitySetQC.txt'], 'rt');
   if fid == -1, return; end
-  
   params = textscan(fid, '%s%f%f%d8%d8', 'delimiter', ',', 'commentStyle', '%');
   fclose(fid);
 catch e
@@ -253,15 +277,45 @@ catch e
   rethrow(e);
 end
 
-% look for an instrument match
-for i=1:length(params{1})
-    if ~isempty(strfind(lower(instrument), lower(params{1}{i})))
-        firstTiltThreshold  = params{2}(i);
-        secondTiltThreshold = params{3}(i);
-        firstTiltflag  = params{4}(i);
-        secondTiltflag = params{5}(i);
-        return;
-    end
+names = params{1};
+nlen = length(names);
+
+%first try to match first name
+for k=1:nlen
+  if strcmpi(instrument,names{k})
+    matchedName = names{k};
+    firstTiltThreshold = params{2}(k);
+    secondTiltThreshold = params{3}(k);
+    firstTiltflag = params{4}(k);
+    secondTiltflag = params{5}(k);
+    return
+  end
+end
+
+%then try to match the instrument name to any of options
+submatch = contains(names,instrument,'IgnoreCase',true);
+if any(submatch)
+  k = find(submatch,1);
+  matchedName = names{k};
+  firstTiltThreshold = params{2}(k);
+  secondTiltThreshold = params{3}(k);
+  firstTiltflag = params{4}(k);
+  secondTiltflag = params{5}(k);
+  return
+end
+
+%finally, try to match specific options on the table as substrings in the instrument
+%name - useful when loading from netcdfs
+for k=1:nlen
+  invmatch = contains(instrument,names{k},'IgnoreCase',true);
+  if invmatch
+    matchedName = names{k};
+    firstTiltThreshold = params{2}(k);
+    secondTiltThreshold = params{3}(k);
+    firstTiltflag = params{4}(k);
+    secondTiltflag = params{5}(k);
+    return
+  end
 end
 
 end
