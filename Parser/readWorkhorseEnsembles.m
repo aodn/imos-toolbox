dd47547ded807a69b388df5a7ce4416034c9481f
Hugo Oliveira <ocehugo@gmail.com>
2021-04-01 15:11:08 +1100

    fix(readWorkhorseEnsembles): compatibility revert
    This fix a compatibility problem introduced recently
    by using the de2bi function.
    This function may not be avaiable to everyone. Hence,
    the old bit wrangling was restore for all variables affected.

--- Parser/readWorkhorseEnsembles.m
+++ Parser/readWorkhorseEnsembles.m
@@ -383,8 +383,17 @@ function [sect, len] = parseFixedLeader(data, idx, cpuEndianness)
 % 0 1 0 0 - - - - 4-BEAM JANUS CONFIG
 % 0 1 0 1 - - - - 5-BM JANUS CFIG DEMOD)
 % 1 1 1 1 - - - - 5-BM JANUS CFIG.(2 DEMD)
-  LSB = logical(de2bi(data(idx+4),8,'left-msb'));
-  MSB = logical(de2bi(data(idx+5),8,'left-msb'));
+%
+LSB = dec2bin(double(data(idx+4)));
+MSB = dec2bin(double(data(idx+5)));
+while(size(LSB, 2) < 8), LSB = [repmat('0', size(LSB, 1), 1) LSB]; end
+while(size(MSB, 2) < 8), MSB = [repmat('0', size(MSB, 1), 1) MSB]; end
+LSB = bin2logical(LSB);
+MSB = bin2logical(MSB);
+% compatibility layer, code above is equivalent to:
+%LSB = logical(de2bi(data(idx+4),8,'left-msb'));
+%MSB = logical(de2bi(data(idx+5),8,'left-msb'));
+%
   sect.systemConfiguration = [LSB MSB];
   sect.realSimFlag         = double(data(idx+6));
   sect.lagLength           = double(data(idx+7));
@@ -410,12 +419,29 @@ function [sect, len] = parseFixedLeader(data, idx, cpuEndianness)
   % xxxxx1xx = TILTS (PITCH AND ROLL) USED IN SHIP OR EARTH TRANSFORMATION
   % xxxxxx1x = 3-BEAM SOLUTION USED IF ONE BEAM IS BELOW THE CORRELATION THRESHOLD SET BY THE WC command
   % xxxxxxx1 = BIN MAPPING USED
-  sect.coordinateTransform = logical(de2bi(data(idx+25),8,'left-msb'));
+  %
+  ct = dec2bin(double(data(idx+25)));
+  while(size(ct, 2) < 8), ct = [repmat('0', size(ct,1), 1) ct]; end
+  ct = bin2logical(ct);
+  sect.coordinateTransform = ct;
+  %compatibility layer, code above is equivalent to:
+  %sect.coordinateTransform = logical(de2bi(data(idx+25),8,'left-msb'));
   block                    = indexData(data, idx+26, idx+29, 'int16', cpuEndianness)';
   sect.headingAlignment    = block(:,1);
   sect.headingBias         = block(:,2);
-  sect.sensorSource        = logical(de2bi(data(idx+30),8,'left-msb'));
-  sect.sensorsAvailable    = logical(de2bi(data(idx+31),8,'left-msb'));
+  %
+  ss = dec2bin(double(data(idx+30)));
+  while(size(ss, 2) < 8), ss = [repmat('0', size(ss,1), 1) ss]; end
+  ss = bin2logical(ss);
+  sect.sensorSource = ss;
+  %compatibility layer, code above is equivalent to:
+  %sect.sensorSource        = logical(de2bi(data(idx+30),8,'left-msb'));
+  sa = dec2bin(double(data(idx+31)));
+  while(size(sa, 2) < 8), sa = [repmat('0', size(sa,1), 1) sa]; end
+  sa = bin2logical(sa);
+  sect.sensorsAvailable = sa;
+  %compatibility layer, code above is equivalent to:
+  %sect.sensorsAvailable    = logical(de2bi(data(idx+31),8,'left-msb'));
   block                    = indexData(data, idx+32, idx+35, 'uint16', cpuEndianness)';
   sect.bin1Distance        = block(:,1);
   sect.xmitPulseLength     = block(:,2);
