4f11f093a27d2d1749cc103f69491d53ea520f1f
Hugo Oliveira <ocehugo@gmail.com>
2021-04-01 14:39:31 +1100

    fix(genSampleDataDesc): remove license body

--- Util/genSampleDataDesc.m
+++ Util/genSampleDataDesc.m
@@ -17,24 +17,6 @@ function desc = genSampleDataDesc(sam, detailLevel)
 %
 % Author: Paul McCarthy <paul.mccarthy@csiro.au>
 %
-
-%
-% Copyright (C) 2017, Australian Ocean Data Network (AODN) and Integrated
-% Marine Observing System (IMOS).
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation version 3 of the License.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-% GNU General Public License for more details.
-
-% You should have received a copy of the GNU General Public License
-%along with this program.
-% If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
-%
 narginchk(1,2);
 
 if ~isstruct(sam), error('sam must be a struct'); end
