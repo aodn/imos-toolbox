3881f2aef015b2540d324c7fc8f9459324e34976
Bec <rebecca.cowley@csiro.au>
2021-12-03 12:28:44 +1100

    ADCP thresholds plots updates

--- test/single_pingADCPtest.m
+++ test/single_pingADCPtest.m
@@ -6,61 +6,82 @@
 %
 % Bec Cowley, May/June, 2020
 %Have updated workhorseParse to do beam to ENU conversion
+% 2021: adapted to review screening of single-ping data with fish detection
+% and correlation magnitude
 
 % Set up:
 clear
   % get the toolbox execution mode
   mode = readProperty('toolbox.mode');
-toolboxVersion = ['2.6.6 - ' computer];
+toolboxVersion = ['2.6.13 - ' computer];
 auto = false;
 iMooring = [];
 nonUTCRawData = importManager(toolboxVersion, auto, iMooring);
-
-
-% add an index field to each data struct
+%% first do mapping and an ENU conversion so we can see velocities in the screening plots
+%run using the pp window to ensure flags are handled properly;
+% run these routines:
+% adcpBinMappingPP
+% adcpWorkhorseCorrMagPP - RDI recommended threshold = 64
+% adcpWorkhorseEchoRangePP - RDI recommended threshold = 50
+% adcpWorkhorseVelocityBeam2EnuPP
 for k = 1:length(nonUTCRawData), nonUTCRawData{k}.meta.index = k; end
-
-%% preprocess data
-[ppData, cancel] = preprocessManager(nonUTCRawData, 'qc', mode, false);
-if cancel
-    rawData = nonUTCRawData;
-else
-    rawData = preprocessManager(nonUTCRawData, 'raw',  mode, true);  % only apply TIME to UTC conversion pre-processing routines, auto is true so that GUI only appears once
-end
-
-% save data set selection
-setIdx = 1:length(ppData);
-
+% preprocess data
+ [sden, cancel] = preprocessManager(nonUTCRawData, 'qc', mode, false); 
+% magneticDeclinationPP
 %% Determine thresholds using some low-overhead plots:
-adcpThresholds
+
+%first check the single-ping screening thresholds (echo range and
+%corrMagPP)
+% If these are not suitable, need to adjust in the pp step (next cell).
+% Might need to delete the *.ppp files in the raw_data folder before
+% running PP as the toolbox will use whatever is in the ppp file, regardless
+% of what you type into the starting box.
+
+adcpScreeningThresholds(sden,1)
 return
-%% Run autoQC tests, apply selected thresholds here:
+%% Now re-do with ensemble averaging; apply pp tools in this order:
+% adcpBinMappingPP
+% adcpWorkhorseCorrMagPP - RDI recommended threshold = 64
+% adcpWorkhorseEchoRangePP - RDI recommended threshold = 50
+% adcpWorkhorseVelocityBeam2EnuPP
+% magneticDeclinationPP
+% adcpWorkhorseRDIensembleAveragingPP - adjust the averaging interval,
+%           default = 60 minutes
+% depthPP
+
+% preprocess data
+ [ppData, cancel] = preprocessManager(nonUTCRawData, 'qc', mode, false); 
+%  [nnData, cancel] = preprocessManager(nonUTCRawData, 'qc', mode, false); 
+
+%%
+% now look at ensemble-averaged data for appropriate autoQC thresholds to
+% be applied in the autoQCManager step.
+%error velocity histograms etc
+%cmag already screened. Only apply if not done in single ping screening
+%above, combined with surface test
+
+% adcpEnsemblesThresholds(ppData,inst_index,ervthreshold,hvel,vvel,tilt,echo amp,cmagthreshold)
+adcpEnsemblesThresholds(ppData,2,0.5,1.5,0.1,40,[],80)
+
+return
+%% Run autoQC tests, apply selected thresholds to see impact:
 % do one at a time so that the results plot shows the fails for each
 % individual test.
 % run QC routines over raw data
-aqc = autoQCManager(ppData(setIdx));
-
-% if user interrupted process, return either pre-processed data (if no QC performed yet) or old QC data
-if isempty(aqc)
-    aqc = autoQCData(setIdx);
-end
+aqc = autoQCManager(ppData);
 
 % otherwise return new QC data
-autoQCData(setIdx) = aqc;
+autoQCData = aqc;
 
-%set the qclevel flag so we are forced to load the historicalQCdataset in
-%GUI
-for Idx = 1:length(setIdx)
-    autoQCData{Idx}.meta.level = 0;
-end
 % now review the results of the flags applied with each test
 % comment out as required
 % adcpThresholdsResults(autoQCData,'surfacetest')
-adcpThresholdsResults(autoQCData,'echorange')
-% adcpThresholdsResults(autoQCData,'erv')
+% adcpThresholdsResults(autoQCData,'vvel')
+% adcpThresholdsResults(autoQCData,'echorange')
+adcpThresholdsResults(autoQCData,'erv')
 % adcpThresholdsResults(autoQCData,'cmag')
 % adcpThresholdsResults(autoQCData,'echo')
-
+return
 %% now we have the thresholds, run the toolbox as is, with the values determined.
 % need to delete the pqc file for the RDIs before doing the imos toobox as
 % I think that the values in pqc are overwriting edits in the
@@ -68,3 +89,4 @@ adcpThresholdsResults(autoQCData,'echorange')
 
 %Next step, export to single ping netcdf and then re-import to matlab, bin
 %average and stack.
+
